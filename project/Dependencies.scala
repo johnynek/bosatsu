ef8b53a963f228bfa81de2dbdeb0c146e41f57f0
Oscar Boykin <oscar@stripe.com>
2017-10-21 11:58:44 -1000

    Update decline

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,5 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
   lazy val cats = "org.typelevel" %% "cats-core" % "1.0.0-MF"
   lazy val fastparse = "com.lihaoyi" %% "fastparse" % "1.0.0"
-  lazy val decline = "com.monovore" %% "decline" % "0.3.0"
+  lazy val decline = "com.monovore" %% "decline" % "0.4.0-M2"
 }
