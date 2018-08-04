96cb0cb9e82503a16db1937f3af95babd8bd03f8
Steven Henry Klaiber Noble <steven.noble@gmail.com>
2018-08-03 20:09:03 -0700

    Generalize the use of NameKind

--- core/src/main/scala/org/bykn/bosatsu/NameKind.scala
+++ core/src/main/scala/org/bykn/bosatsu/NameKind.scala
@@ -1,13 +1,15 @@
 package org.bykn.bosatsu
 
-sealed abstract class NameKind
+sealed abstract class NameKind[T]
 object NameKind {
-  case class Let(value: Expr[(Declaration, Scheme)]) extends NameKind
-  case class Constructor(cn: ConstructorName, dt: DefinedType, scheme: Scheme) extends NameKind
-  case class Import(fromPack: Package.Inferred, originalName: String) extends NameKind
-  case class ExternalDef(pack: PackageName, defName: String, defType: Scheme) extends NameKind
+  case class Let[T](value: Expr[T]) extends NameKind[T]
+  case class Constructor[T](cn: ConstructorName, dt: DefinedType, scheme: Scheme) extends NameKind[T]
+  case class Import[T](fromPack: FixPackage[T], originalName: String) extends NameKind[T]
+  case class ExternalDef[T](pack: PackageName, defName: String, defType: Scheme) extends NameKind[T]
 
-  def externals(from: Package.Inferred): Stream[ExternalDef] = {
+  type FixPackage[T] = Package.FixPackage[Referant, Referant, Program[T, Statement]]
+
+  def externals[T](from: FixPackage[T]): Stream[ExternalDef[T]] = {
     val prog = from.unfix.program
     prog.from.toStream.collect {
       case Statement.ExternalDef(n, _, _, _) =>
@@ -15,30 +17,30 @@ object NameKind {
         // in the TypeEnv
         val scheme = prog.types.values(n)
         val pn = from.unfix.name
-        ExternalDef(pn, n, scheme)
+        ExternalDef[T](pn, n, scheme)
     }
   }
 
-  def apply(from: Package.Inferred, item: String): Option[NameKind] = {
+  def apply[T](from: FixPackage[T], item: String): Option[NameKind[T]] = {
     val prog = from.unfix.program
 
-    def getLet: Option[NameKind] =
+    def getLet: Option[NameKind[T]] =
       prog.getLet(item).map(Let(_))
 
-    def getConstructor: Option[NameKind] = {
+    def getConstructor: Option[NameKind[T]] = {
       val cn = ConstructorName(item)
       prog.types.constructors.get((from.unfix.name, cn)).map { dtype =>
         val scheme = dtype.toScheme(cn).get // this should never throw
-        Constructor(cn, dtype, scheme)
+        Constructor[T](cn, dtype, scheme)
       }
     }
 
-    def getImport: Option[NameKind] =
+    def getImport: Option[NameKind[T]] =
       from.unfix.localImport(item).map { case (originalPackage, i) =>
-        Import(originalPackage, i.originalName)
+        Import[T](originalPackage, i.originalName)
       }
 
-    def getExternal: Option[NameKind] =
+    def getExternal: Option[NameKind[T]] =
       // there should not be duplicate top level names TODO lint for this
       prog.from.toStream.collectFirst {
         case Statement.ExternalDef(n, _, _, _) if n == item =>
@@ -46,7 +48,7 @@ object NameKind {
           // in the TypeEnv
           val scheme = prog.types.values(n)
           val pn = from.unfix.name
-          ExternalDef(pn, item, scheme)
+          ExternalDef[T](pn, item, scheme)
       }
 
     getLet.orElse(
