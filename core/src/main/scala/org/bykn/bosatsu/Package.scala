eb468247f584152a9f3199fe883a88cd61b3a557
Oscar Boykin <oscar.boykin@gmail.com>
2019-03-08 02:23:03 +0000

    remove the imports from interfaces, only give the exports

--- core/src/main/scala/org/bykn/bosatsu/Package.scala
+++ core/src/main/scala/org/bykn/bosatsu/Package.scala
@@ -29,6 +29,9 @@ case class Package[A, B, C, D](
 
   def mapProgram[D1](fn: D => D1): Package[A, B, C, D1] =
     Package(name, imports, exports, fn(program))
+
+  def replaceImports[A1, B1](newImports: List[Import[A1, B1]]): Package[A1, B1, C, D] =
+    Package(name, newImports, exports, program)
 }
 
 object Package {
@@ -37,7 +40,7 @@ object Package {
   type PackageF2[A, B] = PackageF[A, A, B]
   type Parsed = Package[PackageName, Unit, Unit, Statement]
   type Resolved = FixPackage[Unit, Unit, (Statement, ImportMap[PackageName, Unit])]
-  type Interface = FixPackage[NonEmptyList[Referant[Variance]], Referant[Variance], Unit]
+  type Interface = FixPackage[Nothing, Referant[Variance], Unit]
   type Inferred = Package[Interface, NonEmptyList[Referant[Variance]], Referant[Variance], Program[TypeEnv[Variance], TypedExpr[Declaration], Statement]]
 
   /**
@@ -50,9 +53,9 @@ object Package {
   def interfaceOf(inferred: Inferred): Interface =
     Fix[Lambda[A =>
       Package[A,
-        NonEmptyList[Referant[Variance]],
+        Nothing,
         Referant[Variance],
-        Unit]]](inferred.mapProgram(_ => ()))
+        Unit]]](inferred.mapProgram(_ => ()).replaceImports(Nil))
 
   implicit val document: Document[Package[PackageName, Unit, Unit, Statement]] =
     Document.instance[Package.Parsed] { case Package(name, imports, exports, program) =>
