2f51fd6c7e87e51045c7607686bc8f7a7ac4bb7e
Willem van Bergen <willem@vanbergen.org>
2019-03-21 10:27:15 -0700

    Refactor

--- core/src/test/scala/org/bykn/bosatsu/NormalizationTest.scala
+++ core/src/test/scala/org/bykn/bosatsu/NormalizationTest.scala
@@ -4,6 +4,48 @@ import cats.data.{ Validated, ValidatedNel}
 import fastparse.all._
 import org.scalatest.FunSuite
 
+object Normalization {
+
+  def normalizeDeclaration[D](declaration: D): D = {
+    declaration
+  }
+
+  def normalizeLet[D](inferredExpr: (String, RecursionKind, D)): (String, RecursionKind, D) = {
+    println(inferredExpr)
+
+    println(s"let ${inferredExpr._1} = ${inferredExpr._3}")
+    (inferredExpr._1, inferredExpr._2, normalizeDeclaration(inferredExpr._3))
+  }
+
+  def normalizeImport[E](inferredImport: Import[Package.Inferred, E]): Import[Package.Normalized, E] = {
+    println("Import")
+    println(inferredImport.pack)
+    println(inferredImport.items)
+
+    inferredImport
+  }
+
+  def normalizeProgram[T, D, S](inferredProgram: Program[T, D, S]): Program[T, D, S] = {
+    inferredProgram.copy(
+      types = inferredProgram.types,
+      lets  = inferredProgram.lets.map(normalizeLet),
+      from  = inferredProgram.from
+    )
+  }
+
+  def normalizePackage(pkg: Package.Inferred): Package.Normalized = {
+    pkg.copy(
+      program = normalizeProgram(pkg.program)
+    )
+  }
+
+  def normalizePackageMap(pkgMap: PackageMap.Inferred): PackageMap.Normalized = {
+    PackageMap(pkgMap.toMap.map { case (name, pkg) => {
+      (name, Normalization.normalizePackage(pkg))
+    }})
+  }
+}
+
 class NormalizationTest extends FunSuite {
 
   def resolveThenInfer(ps: Iterable[Package.Parsed]): ValidatedNel[PackageError, PackageMap.Inferred] =
@@ -18,75 +60,62 @@ class NormalizationTest extends FunSuite {
         sys.error(s"failed to parse: $s: $exp at $idx with trace: ${extra.traced.trace}")
     }
 
+//   test("package import normalization") {
+//     val p1 = parse(
+// """
+// package Foo
+// export [ main ]
 
+// main = 1
+// """)
 
-  def normalizeLet[D](inferredExpr: (String, RecursionKind, D)) = {
-    println(inferredExpr)
+//     val p2 = parse(
+// """
+// package Foo2
+// import Foo [ main as mainFoo ]
+// export [ main, ]
 
-    println(s"let ${inferredExpr._1} = ${inferredExpr._3}")
-    inferredExpr.copy()
-  }
+// main = mainFoo
+// """)
 
-  def normalizeImport[E](inferredImport: Import[Package.Inferred, E]): Import[Package.Normalized, E] = {
-    println("Import")
-    println(inferredImport.pack)
-    println(inferredImport.items)
+//     val (_, validatedPackageMap) = PackageMap.resolveThenInfer(List(((), p1), ((), p2)))
+//     val packageMap = validatedPackageMap match {
+//       case Validated.Valid(rpm) => rpm
+//       case Validated.Invalid(err) => fail(err.toString)
+//     }
 
-    Import[Package.Inferred, E](
-      normalizePackage(Package.interfaceOf(inferredImport.pack)),
-      inferredImport.items
-    )
-  }
+//     succeed
+//   }
 
-  def normalizeProgram[T, D, S](inferredProgram: Program[T, D, S]): Program[T, D, S] = {
-    inferredProgram.copy(
-      types = inferredProgram.types,
-      lets  = inferredProgram.lets.map(normalizeLet),
-      from  = inferredProgram.from
-    )
-  }
-
-  def normalizePackage(pkg: Package.Inferred): Package.Normalized = {
-    pkg.copy(
-      name = pkg.name,
-      imports = pkg.imports.map(normalizeImport),
-      exports = pkg.exports,
-      program = normalizeProgram(pkg.program)
-    )
-  }
 
   test("simple package normalizes") {
     val p1 = parse(
 """
-package Foo
-export [ main ]
+package Willem/Foo
 
-main = 1
+struct Pair(first, second)
+
+def bar(x):
+  baz = \y -> Pair(x, y)
+  baz(10)
+
+main = bar(5)
 """)
 
-    val p2 = parse(
-"""
-package Foo2
-import Foo [ main as mainFoo ]
-export [ main, ]
-
-main = mainFoo
-""")
-
-    val (_, validatedPackageMap) = PackageMap.resolveThenInfer(List(((), p1), ((), p2)))
+    val (_, validatedPackageMap) = PackageMap.resolveThenInfer(List(((), p1)))
     val packageMap = validatedPackageMap match {
       case Validated.Valid(rpm) => rpm
       case Validated.Invalid(err) => fail(err.toString)
     }
 
-    val normalizedMap = PackageMap(packageMap.toMap.map { case (name, pkg) => {
-      (name, normalizePackage(pkg))
-    }})
+    // println("Input packageMap")
+    // println(packageMap)
 
-    println("Input packageMap")
-    println(packageMap)
+    val normalizedMap = Normalization.normalizePackageMap(packageMap)
 
-    println("Output packageMap")
-    println(normalizedMap)
+    // println("Output packageMap")
+    // println(normalizedMap)
+
+    succeed
   }
 }
