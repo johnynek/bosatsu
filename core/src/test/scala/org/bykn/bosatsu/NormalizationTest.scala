fafb9ed84b119201aa264e1e518291dc8be889cb
Steven Henry Klaiber Noble <steven.noble@gmail.com>
2019-04-04 10:30:47 -0700

    A good simple normalization test

--- core/src/test/scala/org/bykn/bosatsu/NormalizationTest.scala
+++ core/src/test/scala/org/bykn/bosatsu/NormalizationTest.scala
@@ -7,48 +7,23 @@ class NormalizationTest extends FunSuite {
   import NormalExpression._
   import Lit._
   import Normalization._
-  //   test("package import normalization") {
-//     val p1 = parse(
-// """
-// package Foo
-// export [ main ]
 
-// main = 1
-// """)
-
-//     val p2 = parse(
-// """
-// package Foo2
-// import Foo [ main as mainFoo ]
-// export [ main, ]
-
-// main = mainFoo
-// """)
-
-//     val (_, validatedPackageMap) = PackageMap.resolveThenInfer(List(((), p1), ((), p2)))
-//     val packageMap = validatedPackageMap match {
-//       case Validated.Valid(rpm) => rpm
-//       case Validated.Invalid(err) => fail(err.toString)
-//     }
-
-//     succeed
-//   }
-
-
-  test("simple package normalizes") {
-      normalizeTest(
-        List("""
-package Willem/Foo
-
+/*
 struct Pair(first, second)
 
 def bar(x):
   baz = \y -> Pair(x, y)
   baz(10)
+*/
 
-main = bar(5)
+  test("simple package normalizes") {
+      normalizeTest(
+        List("""
+package NormTest/String
+
+main = "aa"
 """
-        ), "Willem/Foo", NormalExpressionTag(Literal(Str("aa")), Set())
+        ), "NormTest/String", NormalExpressionTag(Literal(Str("aa")), Set())
       )
   }
 }
