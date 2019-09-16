5507f4054ae8bccdc0fca7de3ce361acd6b9a6e9
Oscar Boykin <oscar.boykin@gmail.com>
2019-09-15 18:42:50 -1000

    run elm-format

--- elmui/src/Main.elm
+++ elmui/src/Main.elm
@@ -1,9 +1,9 @@
 port module Main exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, button, div, input, text, textarea)
+import Html exposing (Attribute, Html, button, div, input, text, textarea)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+import Html.Events exposing (onClick, onInput)
 
 
 
@@ -11,52 +11,70 @@ import Html.Events exposing (onInput, onClick)
 
 
 main =
-  Browser.element { init = init, subscriptions = subs, update = update, view = view  }
+    Browser.element { init = init, subscriptions = subs, update = update, view = view }
+
+
 
 -- MODEL
 
+
 type alias Model =
-  { code : String,
-    result: String
-  }
+    { code : String
+    , result : String
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  ({ code = "", result = "" }, Cmd.none)
+    ( { code = "", result = "" }, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS and PORTS
 
+
 port toJS : String -> Cmd m
+
+
 port toElm : (String -> m) -> Sub m
 
+
 subs : Model -> Sub Msg
-subs _ = toElm Receive
+subs _ =
+    toElm Receive
+
+
 
 -- UPDATE
 
-type Msg
-  = CodeEdit String
-  | Receive String
-  | Evaluate
 
-update : Msg -> Model -> (Model, Cmd Msg)
+type Msg
+    = CodeEdit String
+    | Receive String
+    | Evaluate
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    CodeEdit newCode ->
-      ({ model | code = newCode }, Cmd.none)
-    Receive res ->
-      ({ model | result = res }, Cmd.none)
-    Evaluate ->
-      ({ model | result = "working..." }, toJS(model.code))
+    case msg of
+        CodeEdit newCode ->
+            ( { model | code = newCode }, Cmd.none )
+
+        Receive res ->
+            ( { model | result = res }, Cmd.none )
+
+        Evaluate ->
+            ( { model | result = "working..." }, toJS model.code )
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ textarea [ placeholder "bosatsu code here", value model.code, onInput CodeEdit ] []
-    , button [ onClick Evaluate ] [ text "go" ]
-    , div [] [ text model.result ]
-    ]
+    div []
+        [ textarea [ placeholder "bosatsu code here", value model.code, onInput CodeEdit ] []
+        , button [ onClick Evaluate ] [ text "go" ]
+        , div [] [ text model.result ]
+        ]
