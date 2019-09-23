7ae84b417a06e4a98419a29c64199b94daae0817
P. Oscar Boykin <johnynek@users.noreply.github.com>
2019-09-22 19:06:49 -1000

    print the checksum of the deploy key (#369)
    * print the checksum of the deploy key
    
    * fix env var

--- elmui/build.sh
+++ elmui/build.sh
@@ -6,7 +6,9 @@ then
   # install elm on travis
   npm install -g elm
   echo "About to set up deploy key"
-  echo $DEPLOY_TRAVIS | base64 -d > bosatsu_deploy_key
+  echo $DEPLOY_KEY | base64 -d > bosatsu_deploy_key
+  # make sure the key was created correctly (cksum is fine, and don't leak more than we need to)
+  cksum bosatsu_deploy_key
 fi
 
 cd elmui
