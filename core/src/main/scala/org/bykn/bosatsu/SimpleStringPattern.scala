dddca89e79ffc9bfc2c1aee337dd00b6e2f1e173
P. Oscar Boykin <boykin@pobox.com>
2019-12-01 10:30:15 -1000

    Add SimpleStringPattern to model string pattern matching

---
+++ core/src/main/scala/org/bykn/bosatsu/SimpleStringPattern.scala
@@ -0,0 +1,126 @@
+package org.bykn.bosatsu
+
+object SimpleStringPattern {
+  sealed trait Pattern {
+    def unapply(str: String): Option[Map[String, String]] =
+      matches(this, str)
+  }
+
+  sealed trait Pattern1 extends Pattern
+
+  case class Lit(str: String) extends Pattern1
+  case class Var(name: String) extends Pattern1
+  case object Wildcard extends Pattern1
+  case class Cat(head: Pattern1, tail: Pattern) extends Pattern
+
+  /**
+   * If two vars are adjacent, the left one will always match empty string
+   * this normalize just removes the left var
+   */
+  def normalize(p: Pattern): Pattern =
+    p match {
+      case lvw@(Lit(_) | Var(_) | Wildcard) => lvw
+      case Cat(Var(_) | Wildcard, tail@Cat(Var(_) | Wildcard, _)) =>
+        normalize(tail)
+      case Cat(h, tail) =>
+        Cat(h, normalize(tail))
+    }
+
+
+  def render(p: Pattern, vars: Map[String, String]): Option[String] =
+    p match {
+      case Lit(str) => Some(str)
+      case Var(x) => vars.get(x)
+      case Wildcard => None
+      case Cat(head, rest) =>
+        for {
+          hstr <- render(head, vars)
+          tstr <- render(rest, vars)
+        } yield hstr ++ tstr
+    }
+
+  object Pattern {
+    def apply(str: String): Pattern = {
+      import fastparse.all._
+
+      val varWild = Identifier.nameParser.map { n => Var(n.asString) } | P("_").map(_ => Wildcard)
+      val ppat = StringUtil.interpolatedString('\'', P("${"), varWild, P("}"))
+
+      def toPat(l: List[Either[Pattern1, (Region, String)]]): Pattern =
+        l match {
+          case Nil => Lit("")
+          case Left(p1) :: tail =>
+            Cat(p1, toPat(tail))
+          case Right((_, lit)) :: tail =>
+            Cat(Lit(lit), toPat(tail))
+        }
+
+      ppat.map(toPat(_)).parse(str) match {
+        case Parsed.Success(pat, _) => pat
+        case other => sys.error(s"could not parse: $other")
+      }
+    }
+  }
+
+  private[this] val sme = Some(Map.empty[String, String])
+
+  def matches(p: Pattern, str: String): Option[Map[String, String]] =
+    p match {
+      case Lit(s0) =>
+        if (s0 == str) sme
+        else None
+      case Var(n) => Some(Map(n -> str))
+      case Wildcard => sme
+      case Cat(Lit(s0), rest) =>
+        if (str.startsWith(s0)) matches(rest, str.drop(s0.length))
+        else None
+      case Cat(Var(n), rest) =>
+        matchEnd(rest, str) match {
+          case None => None
+          case Some((offset, m1)) =>
+            if (m1.contains(n)) Some(m1)
+            else {
+              val s0 = str.substring(0, offset)
+              Some(m1.updated(n, s0))
+            }
+        }
+      case Cat(Wildcard, rest) =>
+        matchEnd(rest, str).map(_._2)
+    }
+
+  // return the offset in str of the first match of p
+  def matchEnd(p: Pattern, str: String): Option[(Int, Map[String, String])] =
+    p match {
+      case Lit(s0) =>
+        if (str.endsWith(s0)) Some((str.length - s0.length, Map.empty))
+        else None
+      case Var(n) => Some((0, Map(n -> str)))
+      case Wildcard => Some((0, Map.empty))
+      case Cat(Lit(""), rest) => matchEnd(rest, str)
+      case Cat(Lit(s0), rest) =>
+        val idx0 = str.indexOf(s0)
+        if (idx0 < 0) None
+        else {
+          // there is at least one match
+          val nextStr = str.drop(idx0 + s0.length)
+          matches(rest, nextStr) match {
+            case Some(res) => Some((idx0, res))
+            case None =>
+              // if r1 is no good, try searching at idx0 + 1
+              val nextIdx = idx0 + 1
+              if (nextIdx < str.length) {
+                val str1 = str.substring(idx0 + 1)
+                matchEnd(p, str1).map { case (idx, m) => (idx + idx0 + 1, m) }
+              }
+              else None
+            }
+        }
+      case Cat(Var(n), rest) =>
+        matchEnd(rest, str).map { case res@(idx, m) =>
+          if (m.contains(n)) res
+          else (0, m.updated(n, str.substring(0, idx)))
+        }
+      case Cat(Wildcard, rest) =>
+        matchEnd(rest, str).map { case (_, m) => (0, m) }
+    }
+}
---
+++ core/src/test/scala/org/bykn/bosatsu/SimpleStringPatternTest.scala
@@ -0,0 +1,137 @@
+package org.bykn.bosatsu
+
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.prop.PropertyChecks.{ forAll, PropertyCheckConfiguration }
+import org.scalatest.FunSuite
+
+class SimpleStringPatternTest extends FunSuite {
+  import SimpleStringPattern._
+
+  implicit val generatorDrivenConfig =
+    //PropertyCheckConfiguration(minSuccessful = 50000)
+    PropertyCheckConfiguration(minSuccessful = 5000)
+    //PropertyCheckConfiguration(minSuccessful = 5)
+
+  def genPattern(names: Set[String]): Gen[(Set[String], Pattern)] = {
+    val lit: Gen[(Set[String], Pattern1)] =
+      Gen.identifier.map { str => (names, Lit(str)) }
+
+    val varP: Gen[(Set[String], Pattern1)] =
+      Gen.identifier.filter { s => !names(s) }.map { n => (names + n, Var(n)) }
+
+    val cat = for {
+      (n1, ph) <- Gen.oneOf(lit, varP)
+      (n2, ptail) <- genPattern(n1)
+    } yield (n2, Cat(ph, ptail))
+
+    Gen.frequency(
+      (2, lit),
+      (1, Gen.const((names, Wildcard))),
+      (2, varP),
+      (15, cat))
+  }
+
+  def unvar(p: Pattern): String =
+    p match {
+      case Lit(str) => str
+      case Var(_) | Wildcard => ""
+      case Cat(h, tail) => unvar(h) ++ unvar(tail)
+    }
+
+  def unwild(p: Pattern): Pattern =
+    p match {
+      case l@(Lit(_) | Var(_)) => l
+      case Wildcard => Lit("")
+      case Cat(lv@(Lit(_) | Var(_)), tail) => Cat(lv, unwild(tail))
+      case Cat(Wildcard, tail) => unwild(tail)
+    }
+
+  def unlit(p: Pattern): Pattern =
+    p match {
+      case Lit(_) => Wildcard
+      case vw@(Wildcard | Var(_)) => vw
+      case Cat(Lit(_), tail) => unlit(tail)
+      case Cat(vw@(Wildcard | Var(_)), tail) => unlit(tail)
+    }
+
+  implicit val arbPattern: Arbitrary[Pattern] =
+    Arbitrary(genPattern(Set.empty).map(_._2))
+
+  test("matched patterns round trip when rendered") {
+    forAll(arbPattern.arbitrary, Gen.identifier) { (p0: Pattern, str: String) =>
+      val p = unwild(p0)
+      matches(p, str) match {
+        case None => ()
+        case Some(vars) =>
+          // if the unwild matches, the wild must match
+          assert(matches(p0, str).isDefined)
+          val rendered = render(p, vars)
+          assert(rendered == Some(str), s"${rendered.get.length} != ${str.length}, ${rendered.get.toArray.toList.map(_.toInt)} != ${str.toArray.toList.map(_.toInt)}")
+      }
+    }
+  }
+
+  test("if we unvar, we always match") {
+    forAll { p0: Pattern =>
+      val p = unwild(p0)
+      val str = unvar(p)
+      matches(p, str) match {
+        case None => fail("expected to match")
+        case Some(m) => m.foreach { case (k, v) =>
+          assert(v == "", s"key: $k, $v")
+        }
+      }
+    }
+  }
+
+  test("unlit patterns match everything") {
+    forAll { (p0: Pattern, str: String) =>
+      val p = unlit(p0)
+      assert(matches(p, str).isDefined)
+    }
+  }
+
+  test("test some examples") {
+    val p1 = Pattern("'foo${x}bar${y}'")
+
+    assert(matches(p1, "foobar") == Some(Map("x" -> "", "y" -> "")))
+    assert(matches(p1, "foobopbarbopbar") == Some(Map("x" -> "bop", "y" -> "bopbar")))
+    assert(matches(p1, "foobarbar") == Some(Map("x" -> "", "y" -> "bar")))
+    assert(matches(p1, "foobarbarbar") == Some(Map("x" -> "", "y" -> "barbar")))
+
+    val p2 = Pattern("'${front}foo${middle}bar${rest}'")
+
+    assert(matches(p2, "0foo1foo2bar3") == Some(Map("front" -> "0", "middle" -> "1foo2", "rest" -> "3")))
+  }
+
+  test("wildcard on either side is the same as contains") {
+    forAll { (ps: String, s: String) =>
+      assert(matches(Cat(Wildcard, Cat(Lit(ps), Wildcard)), s).isDefined == s.contains(ps))
+    }
+  }
+  test("wildcard on front side is the same as endsWith") {
+    forAll { (ps: String, s: String) =>
+      assert(matches(Cat(Wildcard, Lit(ps)), s).isDefined == s.endsWith(ps))
+    }
+  }
+  test("wildcard on back side is the same as startsWith") {
+    forAll { (ps: String, s: String) =>
+      assert(matches(Cat(Lit(ps), Wildcard), s).isDefined == s.startsWith(ps))
+    }
+  }
+  test("normalized patterns have the same matches as unnormalized ones") {
+    forAll { (p0: Pattern, s: String) =>
+      val pnorm = normalize(p0)
+      val nm = matches(pnorm, s)
+      val m = matches(p0, s)
+      assert(m.isDefined == nm.isDefined)
+      if (m.isDefined) {
+        m.get.keys.foreach { k =>
+          val mv = m.get(k)
+          val nv = nm.get.getOrElse(k, "")
+          assert(mv == nv, s"key: $k $m, $nm")
+        }
+      }
+    }
+  }
+}
