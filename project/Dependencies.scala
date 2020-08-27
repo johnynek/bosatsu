efbff38cdc2a044ebf2ab0aa6d11b68296a0ee37
Scala Steward <me@scala-steward.org>
2020-08-27 08:24:08 +0200

    Update jython-standalone to 2.7.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val fastparse = Def.setting("com.lihaoyi" %%% "fastparse" % "1.0.0")
   lazy val jawnParser = Def.setting("org.typelevel" %%% "jawn-parser" % "0.14.3")
   lazy val jawnAst = Def.setting("org.typelevel" %%% "jawn-ast" % "0.14.3")
-  lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.1")
+  lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.2")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.3.0")
   lazy val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.13.5")
   lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.0.3")
