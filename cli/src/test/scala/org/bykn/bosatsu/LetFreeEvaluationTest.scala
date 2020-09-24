8ece77c0613f44c8f3888e8dfcebf1c9b7bff10f
Steven Klaiber-Noble <steven.noble@gmail.com>
2020-09-24 21:25:51 +0000

    json tests

--- cli/src/test/scala/org/bykn/bosatsu/LetFreeEvaluationTest.scala
+++ cli/src/test/scala/org/bykn/bosatsu/LetFreeEvaluationTest.scala
@@ -1,10 +1,13 @@
 package org.bykn.bosatsu
 
+import cats.Eval
 import cats.data.NonEmptyList
 import java.nio.file.{Path => JPath}
 import java.nio.file.{Paths => JPaths}
 import cats.effect.IO
 import org.scalatest.FunSuite
+import LetFreeEvaluation.{ComputedValue, LazyValue, ExtEnv, Cache}
+import Value.ExternalValue
 
 class LetFreeEvaluationTest extends FunSuite {
   import PathModule.MainCommand.LetFreeEvaluate
@@ -73,4 +76,65 @@ class LetFreeEvaluationTest extends FunSuite {
     letFreeTest(List("euler7", "List", "Bool", "Nat"), "Euler/P7")
   }
 
+  test("LetFreeValue") {
+    implicit val extEnv: ExtEnv = Map()
+    implicit val cache: Cache = None
+    val scope = List(
+      ComputedValue(ExternalValue(100)),
+      ComputedValue(ExternalValue(200)),
+      ComputedValue(ExternalValue(300)),
+      ComputedValue(ExternalValue(400))
+    )
+    val lv = LazyValue(
+      LetFreeExpression.App(
+        LetFreeExpression.Lambda(LetFreeExpression.LambdaVar(1)),
+        LetFreeExpression.LambdaVar(3)
+      ),
+      scope,
+      Eval.later(ExternalValue(100))
+    )
+
+    val computedJson0 = Json.JObject(
+      List(
+        "state" -> Json.JString("computed"),
+        "data" -> Json.JString("ExternalValue(100)")
+      )
+    )
+    val computedJson3 = Json.JObject(
+      List(
+        "state" -> Json.JString("computed"),
+        "data" -> Json.JString("ExternalValue(400)")
+      )
+    )
+
+    assert(scope(0).toJson == computedJson0)
+    assert(scope(3).toJson == computedJson3)
+    assert(lv.cleanedScope == List((0, scope(0)), (3, scope(3))))
+    assert(
+      lv.toJson == Json.JObject(
+        List(
+          "state" -> Json.JString("expression"),
+          "expression" -> Json.JString(
+            "App(Lambda(LambdaVar(1)),LambdaVar(3))"
+          ),
+          "scope" -> Json.JArray(
+            Vector(
+              Json.JArray(
+                Vector(
+                  Json.JNumberStr("0"),
+                  computedJson0
+                )
+              ),
+              Json.JArray(
+                Vector(
+                  Json.JNumberStr("3"),
+                  computedJson3
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  }
 }
