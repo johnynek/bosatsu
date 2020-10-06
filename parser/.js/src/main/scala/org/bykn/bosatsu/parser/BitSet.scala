2fa22dad2ea02115be0127b3ec7825762bc137d2
P. Oscar Boykin (via Travis CI) <johnynek@users.noreply.github.com>
2020-10-06 02:28:18 +0000

    Deploy johnynek/bosatsu to github.com/johnynek/bosatsu.git:gh-pages

--- parser/.js/src/main/scala/org/bykn/bosatsu/parser/BitSet.scala
+++ parser/.js/src/main/scala/org/bykn/bosatsu/parser/BitSet.scala
@@ -9,17 +9,25 @@ object BitSetUtil {
   @inline final val isScalaJvm = false
 
   @inline def isSet(b: BitSet, idx: Int): Boolean =
-    (idx == 0) || ((idx > 0) && b(idx))
+    (idx >= 0) && b(idx)
 
   def bitSetFor(charArray: Array[Char]): BitSet = {
     val min = charArray(0).toInt
     val bs = new BitSet(charArray(charArray.length - 1).toInt + 1 - min)
-    var idx = 1
+    var idx = 0
     while (idx < charArray.length) {
       bs += charArray(idx).toInt - min
       idx += 1
     }
 
     bs
+  }
+
+  // what are all the Chars in these bitsets
+  def union(bs: List[(Int, BitSet)]): Iterable[Char] = {
+    def toIter(m: Int, bs: BitSet): Iterator[Char] =
+      bs.iterator.map { i => (i + m).toChar } ++ Iterator.single(m.toChar)
+
+    bs.flatMap { case (m, bs) => toIter(m, bs) }
   }
 }
