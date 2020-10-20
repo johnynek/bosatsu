6a3db79e5b4dc85b0be1b35c61cd80b484064586
Steven Klaiber-Noble <steven.noble@gmail.com>
2020-10-20 22:23:24 +0000

    external function works

--- core/src/test/scala/org/bykn/bosatsu/ExprFnTest.scala
+++ core/src/test/scala/org/bykn/bosatsu/ExprFnTest.scala
@@ -2,6 +2,7 @@ package org.bykn.bosatsu
 
 import java.math.BigInteger
 import org.scalatest.funsuite.AnyFunSuite
+import cats.data.NonEmptyList
 
 class ExprFnTest extends AnyFunSuite {
   import TestUtils._
@@ -15,19 +16,40 @@ out = [1,2,3].foldLeft(4, add)
 """),
       "Ext/Add",
       Externals(Map.empty),
-      List(v => assert(v.asExternal.toAny == BigInteger.valueOf(10), "should just be a number"))
+      List(v =>
+        assert(
+          v.asExternal.toAny == BigInteger.valueOf(10),
+          "should just be a number"
+        )
+      )
     )
   }
   test("test of custom externals") {
     letFreeEvaluateTest(
       List("""
-package Ext/Add
+package Ext/Sqrt
 
-out = [1,2,3].foldLeft(4, add)   
+external def sqrt(x: Int) -> Int
+
+out = [1,2,3,4].map_List(sqrt).foldLeft(0, add)   
 """),
-      "Ext/Add",
-      Externals(Map.empty),
-      List(v => assert(v.asExternal.toAny == BigInteger.valueOf(10), "should just be a number"))
+      "Ext/Sqrt",
+      Externals(
+        Map(
+          (PackageName(NonEmptyList.of("Ext", "Sqrt")), "sqrt") -> FfiCall.Fn1(
+            v =>
+              Value.ExternalValue(
+                v.asExternal.toAny.asInstanceOf[BigInteger].sqrt()
+              )
+          )
+        )
+      ),
+      List(v =>
+        assert(
+          v.asExternal.toAny == BigInteger.valueOf(5),
+          "should just be a number"
+        )
+      )
     )
   }
 }
