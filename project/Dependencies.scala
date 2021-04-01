1aaa5163d333a3fc99afe17b315ef5a0d3a3a5e0
Scala Steward (via Travis CI) <43047562+scala-steward@users.noreply.github.com>
2021-04-01 01:08:59 +0000

    Deploy johnynek/bosatsu to github.com/johnynek/bosatsu.git:gh-pages

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
-  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.4.2")
+  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.5.0")
   lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "2.4.0")
   lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.1")
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "1.4.0")
@@ -11,6 +11,6 @@ object Dependencies {
   lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.2")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.0")
   lazy val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.15.3")
-  lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.4")
+  lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.6")
   lazy val scalaTestPlusScalacheck = Def.setting("org.scalatestplus" %%% "scalacheck-1-14" % "3.2.2.0")
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,7 +4,7 @@ addSbtPlugin("com.lightbend.paradox" % "sbt-paradox" % "0.9.2")
 addSbtPlugin("com.thesamet" % "sbt-protoc" % "0.99.34")
 addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "1.0.0")
 addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.0.0")
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.5.0")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.5.1")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
 addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.4.0")
 
