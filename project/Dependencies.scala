22cebe791ebb22e3a6ba0d9f3b16ae1e7bf5b939
Scala Steward <scala_steward@virtuslab.com>
2022-07-07 20:32:02 +0000

    Revert commit(s) e7c754ee

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.7.0")
   lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.3.12")
-  lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.8")
+  lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.7")
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "2.2.0")
   lazy val jawnParser = Def.setting("org.typelevel" %%% "jawn-parser" % "1.3.2")
   lazy val jawnAst = Def.setting("org.typelevel" %%% "jawn-ast" % "1.3.2")
