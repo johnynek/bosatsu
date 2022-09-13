626b34dcbb865f7e3c70021d654848453b0f316b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-09-13 17:15:31 +0200

    Update jython-standalone to 2.7.3 (#886)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "2.3.0")
   lazy val jawnParser = Def.setting("org.typelevel" %%% "jawn-parser" % "1.4.0")
   lazy val jawnAst = Def.setting("org.typelevel" %%% "jawn-ast" % "1.4.0")
-  lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.2")
+  lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.3")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.2")
   lazy val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.16.0")
   lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.13")
