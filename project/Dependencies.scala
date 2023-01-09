da1ee9aeaf1aaaca8504df4e90bb0a28f27ccdf1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-09 19:58:38 +0100

    Update cats-parse to 0.3.9 (#920)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.9.0")
   lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.4.4")
-  lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.8")
+  lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.9")
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "2.4.1")
   lazy val jawnParser = Def.setting("org.typelevel" %%% "jawn-parser" % "1.4.0")
   lazy val jawnAst = Def.setting("org.typelevel" %%% "jawn-ast" % "1.4.0")
