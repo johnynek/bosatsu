6dacf8fa77b87579c385bfb06377cd45c3ca3b9f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-16 21:32:21 +0100

    Update cats-effect to 3.4.7 (#933)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,16 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.9.0")
-  lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.4.6")
+  lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.4.7")
   lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.9")
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "2.4.1")
   lazy val jawnParser = Def.setting("org.typelevel" %%% "jawn-parser" % "1.4.0")
   lazy val jawnAst = Def.setting("org.typelevel" %%% "jawn-ast" % "1.4.0")
   lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.3")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.2")
-  lazy val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.17.0")
+  lazy val scalaCheck =
+    Def.setting("org.scalacheck" %%% "scalacheck" % "1.17.0")
   lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.15")
-  lazy val scalaTestPlusScalacheck = Def.setting("org.scalatestplus" %%% "scalacheck-1-17" % "3.2.15.0")
+  lazy val scalaTestPlusScalacheck =
+    Def.setting("org.scalatestplus" %%% "scalacheck-1-17" % "3.2.15.0")
 }
