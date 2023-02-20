f83845987503792f8c6fda44f1db3c31593b124c
Patrick Oscar Boykin <boykin@pobox.com>
2023-02-19 21:09:55 -1000

    Add Dagify and improve Toposort types

--- core/src/main/scala/org/bykn/bosatsu/graph/Toposort.scala
+++ core/src/main/scala/org/bykn/bosatsu/graph/Toposort.scala
@@ -1,6 +1,7 @@
 package org.bykn.bosatsu.graph
 
-import cats.implicits._
+import cats.data.NonEmptyList
+import cats.syntax.all._
 
 object Toposort {
 
@@ -12,30 +13,30 @@ object Toposort {
     // these are the nodes which depend on a cyclic subgraph
     def loopNodes: List[A]
     // This returns the dag portion of the graph
-    def layers: Vector[List[A]]
+    def layers: Vector[NonEmptyList[A]]
     // if this is success, return Some with the layers
-    def toSuccess: Option[Vector[List[A]]] =
+    def toSuccess: Option[Vector[NonEmptyList[A]]] =
       this match {
-        case Success(res) => Some(res)
+        case Success(res, _) => Some(res)
         case Failure(_, _) => None
       }
 
     // true if each layer has exactly one item in it
     def layersAreTotalOrder: Boolean =
-      layers.forall(_.lengthCompare(1) == 0)
+      layers.forall(_.tail.isEmpty)
 
     def isSuccess: Boolean =
       this match {
-        case Success(_) => true
+        case Success(_, _) => true
         case Failure(_, _) => false
       }
 
     def isFailure: Boolean = !isSuccess
   }
-  final case class Success[A](layers: Vector[List[A]]) extends Result[A] {
+  final case class Success[A](layers: Vector[NonEmptyList[A]], nfn: A => List[A]) extends Result[A] {
     def loopNodes = Nil
   }
-  final case class Failure[A](loopNodes: List[A], layers: Vector[List[A]]) extends Result[A]
+  final case class Failure[A](loopNodes: List[A], layers: Vector[NonEmptyList[A]]) extends Result[A]
 
   /**
    * Build a deterministic topological sort
@@ -45,7 +46,7 @@ object Toposort {
    * return a result which tells us the layers of the dag, and the non-dag nodes
    */
   def sort[A: Ordering](n: Iterable[A])(fn: A => List[A]): Result[A] =
-    if (n.isEmpty) Success(Vector.empty)
+    if (n.isEmpty) Success(Vector.empty, fn)
     else {
       // save this to avoid some realloations
       val someZero: Option[Int] = Some(0)
@@ -86,10 +87,13 @@ object Toposort {
           }
 
           // the items are already sorted since we added them in sorted order
-          ary.iterator.map(_.result()).toVector
+          ary.iterator.map { bldr =>
+            // We know each layer must have at least 1 item
+            NonEmptyList.fromListUnsafe(bldr.result())
+          }.toVector
         }
       }
       if (bad) Failure(res.collect { case Left(n) => n }, goodRes)
-      else Success(goodRes)
+      else Success(goodRes, fn)
     }
 }
