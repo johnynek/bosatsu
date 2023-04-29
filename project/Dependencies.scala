b7d13946b00505a18628def4b090c94afa899a4e
Scala Steward <scala_steward@virtuslab.com>
2023-04-29 15:57:09 +0000

    Update cats-effect to 3.4.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,8 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.9.0")
-  lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.4.8")
+  lazy val catsEffect =
+    Def.setting("org.typelevel" %%% "cats-effect" % "3.4.10")
   lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.9")
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "2.4.1")
   lazy val jawnParser = Def.setting("org.typelevel" %%% "jawn-parser" % "1.4.0")
