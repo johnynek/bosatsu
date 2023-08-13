1f72bdcfd97d51dd8bd2a5fc1cb63150c73f8b82
P. Oscar Boykin <johnynek@users.noreply.github.com>
2023-08-13 13:47:01 -1000

    Improve the source embedding macro (#1020)

--- base/src/main/scala/org/bykn/bosatsu/Macro.scala
+++ base/src/main/scala/org/bykn/bosatsu/Macro.scala
@@ -1,20 +1,44 @@
 package org.bykn.bosatsu
 
 import reflect.macros.blackbox.Context
-import scala.io._
-
-import scala.util.{Try => STry}
+import java.io.File
+import scala.io.Source
+import scala.util.control.NonFatal
 
 class Macro(val c: Context) {
   import c._, universe._
-  def smac(file: c.Expr[String]) = file.tree match {
-    case Literal(Constant(s: String)) =>
-      val res = STry(
-        Source.fromFile(s, "UTF-8").getLines().mkString("\n")
-      ).getOrElse(
-        Source.fromFile(s"external/org_bykn_bosatsu/$s", "UTF-8").getLines().mkString("\n")
-      )
-      q"$res"
+  def loadFileInCompileImpl(file: c.Expr[String]): c.Expr[String] = {
+
+    def loadPath(s: String): Option[c.Expr[String]] =
+      try {
+        val f = new File(s)
+        if (f.exists()) {
+          val res = Source.fromFile(s, "UTF-8").getLines().mkString("\n")
+          Some(c.Expr[String](q"$res"))
+        }
+        else {
+          None
+        }
+      }
+      catch {
+        case NonFatal(err) =>
+          c.abort(c.enclosingPosition, s"could not read existing file: $s. Exception: $err")
+      }
+
+    file.tree match {
+      case Literal(Constant(s: String)) =>
+        loadPath(s)
+          .orElse {
+            loadPath(s"external/org_bykn_bosatsu/$s")
+          }
+          .getOrElse {
+            c.abort(
+              c.enclosingPosition,
+              s"no file found at: $s. working directory is ${System.getProperty("user.dir")}")
+          }
+      case otherTree =>
+        c.abort(c.enclosingPosition, s"expected string literal, found: $otherTree")
+    }
   }
 }
 
