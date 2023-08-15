127228f8090e42e74fae7ac422a102a3a6d1230e
Scala Steward <scala_steward@virtuslab.com>
2023-08-15 16:51:11 +0000

    Update cats-core to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
-  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.9.0")
+  lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.10.0")
   lazy val catsEffect =
     Def.setting("org.typelevel" %%% "cats-effect" % "3.5.1")
   lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "0.3.10")
