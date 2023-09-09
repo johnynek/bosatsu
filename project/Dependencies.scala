b18497682cf4745f146b0a5ef87964baf36a21dc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-09 04:26:59 +0200

    Update scalatest to 3.2.17 (#1033)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.3")
   lazy val scalaCheck =
     Def.setting("org.scalacheck" %%% "scalacheck" % "1.17.0")
-  lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.16")
+  lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.17")
   lazy val scalaTestPlusScalacheck =
     Def.setting("org.scalatestplus" %%% "scalacheck-1-17" % "3.2.17.0")
 }
