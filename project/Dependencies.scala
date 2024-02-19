82140cec50a6334e8e3b1fa923732bee1f59b133
Scala Steward <scala_steward@virtuslab.com>
2024-02-19 21:55:55 +0000

    Update scalacheck-1-17 to 3.2.18.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,11 +12,12 @@ object Dependencies {
   lazy val jawnAst = Def.setting("org.typelevel" %%% "jawn-ast" % "1.5.1")
   lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.3")
   lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.0-M10")
-  lazy val munitScalaCheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0-M10")
+  lazy val munitScalaCheck =
+    Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0-M10")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.3")
   lazy val scalaCheck =
     Def.setting("org.scalacheck" %%% "scalacheck" % "1.17.0")
   lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.17")
   lazy val scalaTestPlusScalacheck =
-    Def.setting("org.scalatestplus" %%% "scalacheck-1-17" % "3.2.17.0")
+    Def.setting("org.scalatestplus" %%% "scalacheck-1-17" % "3.2.18.0")
 }
