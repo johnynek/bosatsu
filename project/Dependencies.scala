ddc2773e06e3763d69c6c588e92a302d238a7ee1
Scala Steward <scala_steward@virtuslab.com>
2024-02-23 19:05:23 +0000

    Update ff4s to 0.23.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,12 +7,13 @@ object Dependencies {
     Def.setting("org.typelevel" %%% "cats-effect" % "3.5.3")
   lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "1.0.0")
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "2.4.1")
-  lazy val ff4s = Def.setting("io.github.buntec" %%% "ff4s" % "0.20.0")
+  lazy val ff4s = Def.setting("io.github.buntec" %%% "ff4s" % "0.23.0")
   lazy val jawnParser = Def.setting("org.typelevel" %%% "jawn-parser" % "1.5.1")
   lazy val jawnAst = Def.setting("org.typelevel" %%% "jawn-ast" % "1.5.1")
   lazy val jython = Def.setting("org.python" % "jython-standalone" % "2.7.3")
   lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.0-M10")
-  lazy val munitScalaCheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0-M10")
+  lazy val munitScalaCheck =
+    Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0-M10")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.3")
   lazy val scalaCheck =
     Def.setting("org.scalacheck" %%% "scalacheck" % "1.17.0")
