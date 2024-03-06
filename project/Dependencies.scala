af315833fb95ade027a906a197bd12a8f61a7574
Scala Steward <scala_steward@virtuslab.com>
2024-03-06 17:52:14 +0000

    Update cats-effect to 3.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Dependencies {
   lazy val cats = Def.setting("org.typelevel" %%% "cats-core" % "2.10.0")
   lazy val catsEffect =
-    Def.setting("org.typelevel" %%% "cats-effect" % "3.5.3")
+    Def.setting("org.typelevel" %%% "cats-effect" % "3.5.4")
   lazy val catsParse = Def.setting("org.typelevel" %%% "cats-parse" % "1.0.0")
   lazy val decline = Def.setting("com.monovore" %%% "decline" % "2.4.1")
   lazy val ff4s = Def.setting("io.github.buntec" %%% "ff4s" % "0.24.0")
