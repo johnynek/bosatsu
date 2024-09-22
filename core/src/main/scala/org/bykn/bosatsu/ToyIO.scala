0aa627311216efa0cecdf5008614273eac5c0df2
Patrick Oscar Boykin <boykin@pobox.com>
2024-09-22 11:04:31 -1000

    add reader support

--- core/src/main/scala/org/bykn/bosatsu/ToyIO.scala
+++ core/src/main/scala/org/bykn/bosatsu/ToyIO.scala
@@ -1,95 +1,124 @@
 package org.bykn.bosatsu
 
-sealed abstract class ToyIO[+E, +A]
+import cats.Defer
+
+sealed abstract class ToyIO[-R, +E, +A]
 
 object ToyIO {
-  case class Pure[A](get: A) extends ToyIO[Nothing, A]
-  case class Err[E](get: E) extends ToyIO[E, Nothing]
-  case class FlatMap[E, A, B](init: ToyIO[E, A], fn: A => ToyIO[E, B]) extends ToyIO[E, B]
-  case class RecoverWith[E, E1, A](init: ToyIO[E, A], fn: E => ToyIO[E1, A]) extends ToyIO[E1, A]
+  case class Pure[A](get: A) extends ToyIO[Any, Nothing, A]
+  case class Err[E](get: E) extends ToyIO[Any, E, Nothing]
+  case class FlatMap[R, E, A, B](init: ToyIO[R, E, A], fn: A => ToyIO[R, E, B]) extends ToyIO[R, E, B]
+  case class RecoverWith[R, E, E1, A](init: ToyIO[R, E, A], fn: E => ToyIO[R, E1, A]) extends ToyIO[R, E1, A]
   /**
    * fix(f) = f(fix(f))
    */
-  case class ApplyFix[E, A, B](arg: A, fixed: (A => ToyIO[E, B]) => (A => ToyIO[E, B])) extends ToyIO[E, B] {
-    def step: ToyIO[E, B] =
+  case class ApplyFix[R, E, A, B](arg: A, fixed: (A => ToyIO[R, E, B]) => (A => ToyIO[R, E, B])) extends ToyIO[R, E, B] {
+    def step: ToyIO[R, E, B] =
       // it is temping to simplify fixed(fix(fixed)) == fix(fixed)
       // but fix(af.fixed)(af.arg) == af
       // so that doesn't make progress. We need to apply
       // af.fixed at least once to create a new value
       fixed(Fix(fixed))(arg)
   }
-  case class Fix[E, A, B](fn: (A => ToyIO[E, B]) => (A => ToyIO[E, B])) extends Function1[A, ToyIO[E, B]] {
-    def apply(a: A): ToyIO[E,B] = ApplyFix(a, fn)
+  case class Fix[R, E, A, B](fn: (A => ToyIO[R, E, B]) => (A => ToyIO[R, E, B])) extends Function1[A, ToyIO[R, E, B]] {
+    def apply(a: A): ToyIO[R, E, B] = ApplyFix(a, fn)
   }
+  case class ReadEnv[R]() extends ToyIO[R, Nothing, R]
+  case class RemapEnv[R1, R2, E, A](fn: R1 => R2, io: ToyIO[R2, E, A]) extends ToyIO[R1, E, A]
 
-  implicit class ToyIOMethods[E, A](private val io: ToyIO[E, A]) extends AnyVal {
-    def flatMap[E1 >: E, B](fn: A => ToyIO[E1, B]): ToyIO[E1, B] =
+  implicit class ToyIOMethods[R, E, A](private val io: ToyIO[R, E, A]) extends AnyVal {
+    def flatMap[R1 <: R, E1 >: E, B](fn: A => ToyIO[R1, E1, B]): ToyIO[R1, E1, B] =
       FlatMap(io, fn)
 
-    def map[B](fn: A => B): ToyIO[E, B] = flatMap(a => Pure(fn(a)))
-    def recoverWith[E1](fn: E => ToyIO[E1, A]): ToyIO[E1, A] =
+    def map[B](fn: A => B): ToyIO[R, E, B] = flatMap(a => Pure(fn(a)))
+    def recoverWith[E1](fn: E => ToyIO[R, E1, A]): ToyIO[R, E1, A] =
       RecoverWith(io, fn)
 
-    def run: Either[E, A] = ToyIO.run(io)
+    def remapEnv[R1](fn: R1 => R): ToyIO[R1, E, A] =
+      RemapEnv(fn, io)
+
+    def run(env: R): Either[E, A] = ToyIO.run(env, io)
   }
 
-  val unit: ToyIO[Nothing, Unit] = Pure(())
+  val unit: ToyIO[Any, Nothing, Unit] = Pure(())
 
-  def pure[A](a: A): ToyIO[Nothing, A] = Pure(a)
+  def readEnv[R]: ToyIO[R, Nothing, R] = ReadEnv()
 
-  def defer[E, A](io: => ToyIO[E, A]): ToyIO[E, A] =
+  def pure[A](a: A): ToyIO[Any, Nothing, A] = Pure(a)
+
+  def defer[R, E, A](io: => ToyIO[R, E, A]): ToyIO[R, E, A] =
     unit.flatMap(_ => io)
 
-  def delay[A](a: => A): ToyIO[Nothing, A] =
+  def delay[A](a: => A): ToyIO[Any, Nothing, A] =
     defer(Pure(a))
 
-  def raiseError[E](e: E): ToyIO[E, Nothing] = Err(e)
+  def raiseError[E](e: E): ToyIO[Any, E, Nothing] = Err(e)
 
   // fix(f) = f(fix(f))
-  def fix[E, A, B](recur: (A => ToyIO[E, B]) => (A => ToyIO[E, B])): A => ToyIO[E, B] =
+  def fix[R, E, A, B](recur: (A => ToyIO[R, E, B]) => (A => ToyIO[R, E, B])): A => ToyIO[R, E, B] =
     Fix(recur)
 
-  sealed trait Stack[E, A, E1, A1]
+  sealed trait Stack[R, E, A, E1, A1]
 
-  case class Done[E1, A1, E, A](ev: E1 =:= E, av: A1 =:= A) extends Stack[E1, A1, E, A]
-  case class FMStep[E, E1, A, B, B1](fn: A => ToyIO[E, B], stack: Stack[E, B, E1, B1]) extends Stack[E, A, E1, B1]
-  case class RecStep[E, E1, E2, A, B](fn: E => ToyIO[E1, A], stack: Stack[E1, A, E2, B]) extends Stack[E, A, E2, B]
+  case class Done[R, E1, A1, E, A](ev: E1 =:= E, av: A1 =:= A) extends Stack[R, E1, A1, E, A]
+  case class FMStep[R, E, E1, A, B, B1](fn: A => ToyIO[R, E, B], stack: Stack[R, E, B, E1, B1]) extends Stack[R, E, A, E1, B1]
+  case class RecStep[R, E, E1, E2, A, B](fn: E => ToyIO[R, E1, A], stack: Stack[R, E1, A, E2, B]) extends Stack[R, E, A, E2, B]
+  case class Restore[R, R1, E, A, E1, A1](env: R1, stack: Stack[R1, E1, A1, E, A]) extends Stack[R, E1, A1, E, A]
 
-  def run[E, A](io: ToyIO[E, A]): Either[E, A] = {
+  def run[R, E, A](env: R, io: ToyIO[R, E, A]): Either[E, A] = {
 
     @annotation.tailrec
-    def loop[E1, A1](arg: ToyIO[E1, A1], stack: Stack[E1, A1, E, A]): Either[E, A] =
+    def loop[R1, E1, A1](env: R1, arg: ToyIO[R1, E1, A1], stack: Stack[R1, E1, A1, E, A]): Either[E, A] =
       arg match {
+        case FlatMap(init, fn) =>
+          loop(env, init, FMStep(fn, stack))
         case p @ Pure(get) =>
           stack match {
             case Done(_, av) => Right(av(get))
             case FMStep(fn, stack) =>
-              loop(fn(get), stack)
+              loop(env, fn(get), stack)
             case RecStep(_, stack) =>
-              loop(p, stack)
+              // this isn't an error, ignore recovery
+              loop(env, p, stack)
+            case Restore(env, stack) =>
+              loop(env, p, stack)
           }
         case e @ Err(get) =>
           stack match {
             case Done(ev, _) => Left(ev(get))
             case FMStep(_, stack) =>
-              // unwind the stack
-              loop(e, stack)
+              // this is an error, unwind the stack
+              loop(env, e, stack)
             case RecStep(fn, stack) =>
-              loop(fn(get), stack)
+              loop(env, fn(get), stack)
+            case Restore(env, stack) =>
+              loop(env, e, stack)
           }
-        case FlatMap(init, fn) =>
-          loop(init, FMStep(fn, stack))
-        case rw: RecoverWith[e, e1, a] =>
-          loop(rw.init, RecStep(rw.fn, stack))
-        case af: ApplyFix[e, a, b] =>
+        case rw: RecoverWith[r, e, e1, a] =>
+          loop(env, rw.init, RecStep(rw.fn, stack))
+        case af: ApplyFix[r, e, a, b] =>
           // fixed(fix(fixed)) = fix(fixed)
           // take a step here,
           // this may never terminate, because there is no
           // promise that a general recursive function terminates,
           // but it won't blow the stack
-          loop(af.step, stack)
+          loop(env, af.step, stack)
+        case _: ReadEnv[r] =>
+          // r <:< R
+          loop(env, Pure(env), stack)
+        case remap: RemapEnv[r1, r2, e, a] =>
+          val restore: Stack[r2, E1, A1, E, A] = Restore(env, stack)
+          loop[r2, E1, A1](remap.fn(env), remap.io, restore)
       }
 
-    loop(io, Done[E, A, E, A](implicitly, implicitly))
+    loop(env, io, Done[R, E, A, E, A](implicitly, implicitly))
   }
+
+
+  def loopFromDefer[F[_]: Defer, A, B](fn: (A => F[B]) => (A => F[B])): A => F[B] =
+    new Function1[A, F[B]] { self =>
+      lazy val loop = fn(self)
+
+      def apply(a: A) = Defer[F].defer(loop(a))
+    }
 }
--- core/src/test/scala/org/bykn/bosatsu/ToyIOTest.scala
+++ core/src/test/scala/org/bykn/bosatsu/ToyIOTest.scala
@@ -7,36 +7,48 @@ import org.bykn.bosatsu.ToyIO.Pure
 import org.bykn.bosatsu.ToyIO.Err
 import org.bykn.bosatsu.ToyIO.FlatMap
 import org.bykn.bosatsu.ToyIO.ApplyFix
+import org.bykn.bosatsu.ToyIO.ReadEnv
 import org.bykn.bosatsu.ToyIO.RecoverWith
+import org.bykn.bosatsu.ToyIO.RemapEnv
 
 class ToyIOTest extends munit.ScalaCheckSuite {
 
-  def toEvalT[E, A](toyio: ToyIO[E, A]): EitherT[Eval, E, A] =
+    override def scalaCheckTestParameters =
+      super.scalaCheckTestParameters
+        .withMinSuccessfulTests(20000)
+        .withMaxDiscardRatio(10)
+
+  def toEvalT[R, E, A](env: R, toyio: ToyIO[R, E, A]): EitherT[Eval, E, A] =
     toyio match {
       case Pure(get) => EitherT[Eval, E, A](Eval.now(Right(get)))
       case Err(get) => EitherT[Eval, E, A](Eval.now(Left(get)))
-      case fm: FlatMap[e, a1, a2] =>
-        val first = Eval.defer[Either[e, a1]](toEvalT(fm.init).value)
+      case fm: FlatMap[r, e, a1, a2] =>
+        val first = Eval.defer[Either[e, a1]](toEvalT(env, fm.init).value)
         EitherT(first.flatMap {
           case Right(a1) =>
-            val fn1 = fm.fn.andThen { toyio => Eval.defer(toEvalT[e, a2](toyio).value) }
+            val fn1 = fm.fn.andThen { toyio => Eval.defer(toEvalT[r, e, a2](env, toyio).value) }
             fn1(a1)
           case Left(err) =>
             Eval.now(Left(err))
         })
-      case rw: RecoverWith[e, e1, a] =>
-        val first = Eval.defer[Either[e, a]](toEvalT(rw.init).value)
+      case rw: RecoverWith[r, e, e1, a] =>
+        val first = Eval.defer[Either[e, a]](toEvalT(env, rw.init).value)
         EitherT(first.flatMap {
           case Right(a1) => Eval.now(Right(a1))
           case Left(err) =>
-            val fn1 = rw.fn.andThen { toyio => Eval.defer(toEvalT[e1, a](toyio).value) }
+            val fn1 = rw.fn.andThen { toyio => Eval.defer(toEvalT[r, e1, a](env, toyio).value) }
             fn1(err)
         })
-      case af: ApplyFix[e, a, b] =>
-        lazy val fix: a => ToyIO[e, b] =
+      case af: ApplyFix[r, e, a, b] =>
+        lazy val fix: a => ToyIO[r, e, b] =
           { (a: a) => af.fixed(fix)(a) }
 
-        EitherT(Eval.defer(toEvalT(fix(af.arg)).value))
+        EitherT(Eval.defer(toEvalT(env, fix(af.arg)).value))
+      case _: ReadEnv[r] => EitherT[Eval, E, A](Eval.now(Right(env)))
+      case remap: RemapEnv[r1, r2, e, a] =>
+        val r2 = remap.fn(env)
+        val io = remap.io
+        EitherT(Eval.defer(toEvalT(r2, io).value))
     }
 
   trait Move[A] {
@@ -63,12 +75,14 @@ class ToyIOTest extends munit.ScalaCheckSuite {
     def apply[A](implicit m: Move[A]): Move[A] = m
   }
 
-  def genToy[E: Cogen, A: Cogen: Ordering: Move](genE: Gen[E], genA: Gen[A]): Gen[ToyIO[E, A]] = {
-    lazy val recur = Gen.lzy(genToy(genE, genA))
-    val cogenFn: Cogen[A => ToyIO[E, A]] =
+  def genToy[R: Cogen, E: Cogen, A: Cogen: Ordering: Move](genR: Gen[R], genE: Gen[E], genA: Gen[A]): Gen[ToyIO[R, E, A]] = {
+    lazy val recur = Gen.lzy(genToy[R, E, A](genR, genE, genA))
+    val cogenFn: Cogen[A => ToyIO[R, E, A]] =
       Cogen(_.hashCode.toLong)
 
-    lazy val genFix: Gen[(A => ToyIO[E, A]) => (A => ToyIO[E, A])] =
+    val envToA: Gen[R => A] = Gen.function1[R, A](genA)
+
+    lazy val genFix: Gen[(A => ToyIO[R, E, A]) => (A => ToyIO[R, E, A])] =
       Gen.zip(genA, Gen.oneOf(true, false), genA, genE).map { case (cut, lt, result, err) =>
       
         val ord = implicitly[Ordering[A]]
@@ -90,13 +104,17 @@ class ToyIOTest extends munit.ScalaCheckSuite {
       Gen.function1(Gen.function1(recur)(Cogen[A]))(cogenFn)
 
     Gen.frequency(
-      2 -> genA.map(ToyIO.pure(_)),
-      2 -> genE.map(ToyIO.raiseError(_)),
-      1 -> Gen.zip(recur, Gen.function1[A, ToyIO[E, A]](recur)).map { case (io, fn) =>
+      1 -> genA.map(ToyIO.pure(_)),
+      1 -> genE.map(ToyIO.raiseError(_)),
+      1 -> envToA.map { fn => ToyIO.readEnv[A].remapEnv(fn) },
+      5 -> Gen.zip(recur, Gen.function1[A, ToyIO[R, E, A]](recur)).map { case (io, fn) =>
         io.flatMap(fn)  
       },
-      1 -> Gen.zip(recur, Gen.function1[E, ToyIO[E, A]](recur)).map { case (io, fn) =>
+      1 -> Gen.zip(recur, Gen.function1[E, ToyIO[R, E, A]](recur)).map { case (io, fn) =>
         io.recoverWith(fn)  
+      },
+      1 -> Gen.zip(recur, Gen.function1[R, R](genR)).map { case (io, fn) =>
+        io.remapEnv(fn)  
       },
       1 -> Gen.zip(genA, genFix).map { case (a, fn) => ToyIO.fix(fn)(a) }
     )
@@ -106,14 +124,26 @@ class ToyIOTest extends munit.ScalaCheckSuite {
   val bools = Gen.oneOf(true, false)
 
   property("evaluation of ToyIO via Eval matches E=Byte, A=Byte") {
-    Prop.forAll(genToy(bytes, bytes)) { toyio =>
-      assertEquals(toyio.run, toEvalT(toyio).value.value)  
+    Prop.forAll(bytes, genToy(bytes, bytes, bytes)) { (env, toyio) =>
+      assertEquals(toyio.run(env), toEvalT(env, toyio).value.value)  
     }
   }
 
   property("evaluation of ToyIO via Eval matches E=Bool, A=Bool") {
-    Prop.forAll(genToy(bools, bools)) { toyio =>
-      assertEquals(toyio.run, toEvalT(toyio).value.value)  
+    Prop.forAll(bools, genToy(bools, bools, bools)) { (env, toyio) =>
+      assertEquals(toyio.run(env), toEvalT(env, toyio).value.value)  
     }
+  }
+  
+  test("loop from defer works") {
+    val fn = ToyIO.loopFromDefer[Eval, Int, Int] { recur =>
+
+      { (i: Int) =>
+        if (i >= 0) recur(i - 1).map(_ + i)
+        else Eval.now(0)
+      }  
+    }
+
+    assertEquals(fn(100000).value, (0 to 100000).sum)
   }
 }
