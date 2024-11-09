b374c99033880105c63940f49ee2fce30e844d7f
Patrick Oscar Boykin <boykin@pobox.com>
2024-11-09 13:08:55 -1000

    change the representation

--- c_runtime/bosatsu_runtime.c
+++ c_runtime/bosatsu_runtime.c
@@ -16,23 +16,6 @@
 
 // A general structure for a reference counted memory block
 // it is always allocated with len BValue array immediately after
-typedef struct RefCounted {
-    atomic_int ref_count;
-    uint8_t len;
-    union {
-        struct {
-        } struct_value;
-
-        struct {
-            int tag;      // variant tag: TODO can probably limit enum variants
-        } enum_value;
-
-        struct {
-            BClosure func;  // pointer to a static function, first argument is value
-        } closure;
-    } data;
-} RefCounted;
-
 typedef struct _Node {
   RefCounted* value;
   struct _Node* next;
@@ -88,9 +71,6 @@ void init_statics() {
   atomic_store(&statics, empty);
 }
 
-// this is tag set in the length field of RefCounted when we have an external value
-#define EX_VAL_TAG 255
-
 // we *always* put an array of values immediately after rc
 BValue* values_of(RefCounted* rc) {
   return (BValue*)(rc + 1);
@@ -126,20 +106,7 @@ static void release_ref_counted(RefCounted *block) {
     // Decrement the reference count atomically using atomic_fetch_sub
     if (atomic_fetch_sub_explicit(&block->ref_count, 1, memory_order_seq_cst) == 1) {
         // If reference count drops to 0, free the memory
-        int ilen = (int)block->len;
-        BValue* values = values_of(block);
-        if (ilen != EX_VAL_TAG) {
-          for(int i = 0; i < ilen; i++) {
-            release_value(values[i]);
-          }
-        }
-        else {
-          // ilen == EX_VAL_TAG is a tag that we have an external value:
-          void* eval = values[0];
-          FreeFn free_fn = (FreeFn)values[1];
-          free_fn(eval);
-        }
-        free(block);
+        block->free(block);
     }
 }
 
@@ -161,92 +128,33 @@ void release_value(BValue value) {
     }
 }
 
-// Helper function to allocate a RefCounted structure with a specified number of BValues
-static RefCounted* alloc_ref_counted(int len, BValue values[]) {
-    RefCounted* rc = malloc(sizeof(RefCounted) + sizeof(BValue) * len);
-    if (!rc) {
-      // we need to release the values now
-      for (int i = 0; i < len; i++) {
-          release_value(values[i]);
-      }
-      return NULL;
-    }
-    
-    atomic_init(&rc->ref_count, 1);
-    rc->len = len;
-    BValue* rc_values = values_of(rc);
-    for (int i = 0; i < len; i++) {
-        rc_values[i] = values[i];
-    }
-    return rc;
-}
-// we never allocate a struct1, we just use the original type
-
-// this does not clone the args, so we own b1, b2
-BValue alloc_struct2(BValue b1, BValue b2) {
-    BValue args[2] = {b1, b2};
-    return alloc_ref_counted(2, args);
-}
-
-static RefCounted* alloc_ref_counted_enum(uint8_t len, int variant, BValue values[]) {
-    RefCounted* rc = alloc_ref_counted(len, values);
-    if (!rc) return NULL;
-    rc->data.enum_value.tag = variant;
-    return rc;
-}
-
-BValue alloc_enum0(int variant) {
-    BValue args[0] = {};
-    return alloc_ref_counted_enum(0, variant, args);
-}
-
-BValue alloc_enum1(int variant, BValue b1) {
-    BValue args[1] = {b1};
-    return alloc_ref_counted_enum(1, variant, args);
-}
-
-BValue alloc_enum2(int variant, BValue b1, BValue b2) {
-    BValue args[2] = {b1, b2};
-    return alloc_ref_counted_enum(2, variant, args);
-}
-
-// Helper function to allocate a RefCounted structure with a specified number of BValues
-BValue alloc_closure(uint8_t len, BValue captures[], BClosure fn) {
-    RefCounted* rc = alloc_ref_counted(len, captures);
-    if (!rc) return NULL;
-    rc->data.closure.func = fn;
-    return rc;
-}
-
-BValue alloc_external(void* eval, FreeFn free_fn) {
-    RefCounted* rc = malloc(sizeof(RefCounted) + sizeof(BValue) * 2);
-    if (!rc) {
-      // we need to release the values now
-      free_fn(eval);
-      return NULL;
-    }
-    
-    atomic_init(&rc->ref_count, 1);
-    rc->len = EX_VAL_TAG;
-    BValue* values = values_of(rc);
-    values[0] = eval;
-    values[1] = free_fn;
-    return rc;
-}
-
-BValue value_from_pure_fn(BPureFn fn) {
+BValue value_from_pure_fn(BPureFn1 fn) {
   return (BValue)(((uintptr_t)fn) | STATIC_VALUE_TAG);
 }
 
-BValue call_fn(BValue fn, BValue args[]) {
+BValue call_fn1(BValue fn, BValue arg0) {
   if (IS_STATIC_VALUE(fn)) {
-    BPureFn pfn = (BPureFn)fn;
-    return pfn(args);
+    BPureFn1 pfn = (BPureFn1)((uintptr_t)fn & ~TAG_MASK);
+    return pfn(arg0);
   }
   else {
     // this must be a closure:
-    RefCounted* rc = (RefCounted*)fn;
-    return rc->data.closure.func(values_of(rc), args);
+    Closure1Data* rc = (Closure1Data*)fn;
+    BValue* data = closure_data_of(rc);
+    return rc->fn(data, arg0);
+  }
+}
+
+BValue call_fn2(BValue fn, BValue arg0, BValue arg1) {
+  if (IS_STATIC_VALUE(fn)) {
+    BPureFn2 pfn = (BPureFn2)((uintptr_t)fn & ~TAG_MASK);
+    return pfn(arg0, arg1);
+  }
+  else {
+    // this must be a closure:
+    Closure2Data* rc = (Closure2Data*)fn;
+    BValue* data = closure_data_of((Closure1Data*)rc);
+    return ((BClosure2)(rc->fn))(data, arg0, arg1);
   }
 }
 
@@ -256,13 +164,18 @@ BValue get_index(BValue v, int idx) {
 }
 
 void* get_external(BValue v) {
-  RefCounted* rc = (RefCounted*)v;
-  return values_of(rc)[0];
+  External* rc = (External*)v;
+  return rc->external;
 }
 
-int get_variant(BValue v) {
-  RefCounted* rc = (RefCounted*)v;
-  return rc->data.enum_value.tag;
+ENUM_TAG get_variant(BValue v) {
+  if (IS_PURE_VALUE(v)) {
+    return (ENUM_TAG)((uintptr_t)v >> 2);
+  }
+  else {
+    Enum1* real_v = (Enum1*)((uintptr_t) v & ~TAG_MASK);
+    return real_v->tag;
+  }
 }
 
 BValue make_static(BValue v) {
@@ -275,6 +188,8 @@ BValue make_static(BValue v) {
 // Example static
 BValue make_foo();
 static _Atomic BValue __bvalue_foo = NULL;
+// Add this to the main function to construct all
+// the top level values before we start
 BValue foo() {
   return CONSTRUCT(&__bvalue_foo, make_foo);
 }
