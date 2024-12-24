1062e5aa4ee100685dc6bba1def88297aa774d2a
P. Oscar Boykin <johnynek@users.noreply.github.com>
2024-12-24 09:48:12 -1000

    Add OsPlatformId (#1335)

--- core/.jvm/src/main/scala/org/bykn/bosatsu/Platform.scala
+++ core/.jvm/src/main/scala/org/bykn/bosatsu/Platform.scala
@@ -5,4 +5,13 @@ object Platform {
   final val isScalaJs = false
   @inline
   final val isScalaJvm = true
+
+  def detectOs(): OsPlatformId = {
+    val osName0 = System.getProperty("os.name")
+    val osName = osName0.toLowerCase
+    if (osName.contains("win")) OsPlatformId.Windows
+    else if (osName.contains("mac")) OsPlatformId.MacOS
+    else if (osName.contains("linux")) OsPlatformId.Linux
+    else OsPlatformId.Unknown(osName0)
+  }
 }
