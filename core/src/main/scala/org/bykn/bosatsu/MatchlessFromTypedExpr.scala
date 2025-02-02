d6605634ee018d450bff3266c3417d2d3ee1fcca
Oscar Boykin <oscar.boykin@gmail.com>
2025-02-01 19:01:47 -1000

    minor update

--- core/src/main/scala/org/bykn/bosatsu/MatchlessFromTypedExpr.scala
+++ core/src/main/scala/org/bykn/bosatsu/MatchlessFromTypedExpr.scala
@@ -5,11 +5,11 @@ import Identifier.Bindable
 import cats.implicits._
 
 object MatchlessFromTypedExpr {
-  type Compiled = Map[PackageName, List[(Bindable, Matchless.Expr[Unit])]]
+  type Compiled[+A] = Map[PackageName, List[(Bindable, Matchless.Expr[A])]]
   // compile a set of packages given a set of external remappings
   def compile[A](
       pm: PackageMap.Typed[A]
-  )(implicit ec: Par.EC): Compiled = {
+  )(implicit ec: Par.EC): Compiled[Unit] = {
 
     val gdr = pm.getDataRepr
 
