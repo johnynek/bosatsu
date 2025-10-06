6187b6cf8e1bbf72febca74f8ea1c43767deb1fc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-10-06 22:44:23 +0200

    Update scalacheck to 1.19.0 (#1423)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.1.0")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.4")
   lazy val scalaCheck =
-    Def.setting("org.scalacheck" %%% "scalacheck" % "1.18.1")
+    Def.setting("org.scalacheck" %%% "scalacheck" % "1.19.0")
   lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.19")
   lazy val scalaTestPlusScalacheck =
     Def.setting("org.scalatestplus" %%% "scalacheck-1-17" % "3.2.18.0")
