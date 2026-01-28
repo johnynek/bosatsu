c7fbb59e919fa63b81ed04bc49b7de18a5ae3637
P. Oscar Boykin <johnynek@users.noreply.github.com>
2026-01-28 12:43:59 -1000

    Fix issue 1260 (#1509)
    * Fix issue 1260
    
    * remove dead code
    
    * remove dead code
    
    * improve the PR after careful human code review

--- core/src/test/scala/dev/bosatsu/codegen/python/PythonGenTest.scala
+++ core/src/test/scala/dev/bosatsu/codegen/python/PythonGenTest.scala
@@ -1,6 +1,8 @@
 package dev.bosatsu.codegen.python
 
 import dev.bosatsu.Generators.bindIdentGen
+import dev.bosatsu.{Par, TestUtils}
+import dev.bosatsu.codegen.CompilationSource
 import org.scalacheck.Prop.forAll
 
 class PythonGenTest extends munit.ScalaCheckSuite {
@@ -37,5 +39,50 @@ class PythonGenTest extends munit.ScalaCheckSuite {
       }
     """
     assertEquals(PythonGen.evaluatorParser.parseAll(str).map(_ => ()), Right(()))
+  }
+
+  test("top-level lets around lambda avoid closure tuples") {
+    val src =
+      """|def length_String(s):
+         |  def loop(s, acc):
+         |    recur s:
+         |      case "": acc
+         |      case "$.{_}${tail}": loop(tail, acc)
+         |
+         |  loop(s, 0)
+         |""".stripMargin
+
+    TestUtils.checkPackageMap(src) { pm =>
+      Par.withEC {
+        val rendered = PythonGen.renderSource(pm, Map.empty, Map.empty)
+        val doc = rendered(())(TestUtils.testPackage)._2
+        val code = doc.render(120)
+
+        val expected =
+          "\n\n" +
+            """def ___h0(___bs1, ___bacc0):
+    ___a4 = ___bs1
+    ___a6 = ___bacc0
+    ___a1 = 1
+    ___t2 = ___a1 == 1
+    while ___t2:
+        if ___a4 == "":
+            ___a1 = 0
+            ___a2 = ___a6
+        else:
+            ___t1 = 0
+            ___t1 = ___t1 + 1
+            ___a0 = ___a4[1:]
+            ___btail0 = ___a0
+            ___a3 = ___btail0
+            ___a4 = ___a3
+        ___t2 = ___a1 == 1
+    return ___a2
+
+def length_String(___bs0):
+    return ___h0(___bs0, 0)"""
+        assertEquals(code, expected)
+      }
+    }
   }
 }
