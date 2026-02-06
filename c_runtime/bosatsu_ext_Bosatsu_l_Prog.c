2113bc83bcf8544c4c7d6a36a88f1cd414c081b8
P. Oscar Boykin <johnynek@users.noreply.github.com>
2026-02-05 14:04:45 -1000

    Make print, println, and read from stdenv generic on env (#1555)
    * Make print, println, and read from stdenv generic on env
    
    * fix next version
    
    * remove ignore_env
    
    * improve runtime integer code, add c benchmarks (#1563)
    
    * checkpoint tests improvements
    
    * Optimize integer runtime and add c benchmarks
    
    * Revert bitwise/shift twos buffers to main path
    
    * Restore integer optimizations, keep bitwise/shift main path
    
    * run and print runtime.c benchmarks
    
    * Add IOError ADT and unify Prog error types
    
    * Avoid duplicate ENOTSUP case when equal to EOPNOTSUPP
    
    * Add IOError context strings across runtimes
    
    * Include negative read_stdin arg value in IOError context

--- c_runtime/bosatsu_ext_Bosatsu_l_Prog.c
+++ c_runtime/bosatsu_ext_Bosatsu_l_Prog.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
 /*
 # Prog is an ADT with the following values:
@@ -15,6 +16,181 @@
 # RemapEnv(p, f) => (6, p, f)
 # Effect(arg: BValue, f: BValue => BValue) => (7, f)
 */
+
+// Bosatsu/IOError enum variant indices (must match Bosatsu/IOError.bosatsu)
+enum
+{
+  BSTS_IOERR_NotFound = 0,
+  BSTS_IOERR_AccessDenied = 1,
+  BSTS_IOERR_AlreadyExists = 2,
+  BSTS_IOERR_NotDirectory = 3,
+  BSTS_IOERR_IsDirectory = 4,
+  BSTS_IOERR_NotEmpty = 5,
+  BSTS_IOERR_TooManyOpenFiles = 6,
+  BSTS_IOERR_ReadOnlyFileSystem = 7,
+  BSTS_IOERR_CrossDeviceLink = 8,
+  BSTS_IOERR_NoSpace = 9,
+  BSTS_IOERR_QuotaExceeded = 10,
+  BSTS_IOERR_NameTooLong = 11,
+  BSTS_IOERR_InvalidArgument = 12,
+  BSTS_IOERR_InvalidUtf8 = 13,
+  BSTS_IOERR_BadFileDescriptor = 14,
+  BSTS_IOERR_Interrupted = 15,
+  BSTS_IOERR_WouldBlock = 16,
+  BSTS_IOERR_TimedOut = 17,
+  BSTS_IOERR_BrokenPipe = 18,
+  BSTS_IOERR_Unsupported = 19,
+  BSTS_IOERR_Other = 20
+};
+
+static BValue bsts_ioerror_context(const char *context)
+{
+  if (!context)
+  {
+    context = "";
+  }
+  size_t len = strlen(context);
+  return bsts_string_from_utf8_bytes_copy(len, (char *)context);
+}
+
+static BValue bsts_ioerror_known(int variant, const char *context)
+{
+  return alloc_enum1(variant, bsts_ioerror_context(context));
+}
+
+static BValue bsts_ioerror_other(const char *context, int code, const char *msg)
+{
+  if (!msg)
+  {
+    msg = "unknown error";
+  }
+  BValue ctxv = bsts_ioerror_context(context);
+  size_t len = strlen(msg);
+  BValue msgv = bsts_string_from_utf8_bytes_copy(len, (char *)msg);
+  BValue codev = bsts_integer_from_int(code);
+  return alloc_enum3(BSTS_IOERR_Other, ctxv, codev, msgv);
+}
+
+static BValue bsts_ioerror_from_errno(int err, const char *context)
+{
+  switch (err)
+  {
+#ifdef ENOENT
+  case ENOENT:
+    return bsts_ioerror_known(BSTS_IOERR_NotFound, context);
+#endif
+#ifdef EACCES
+  case EACCES:
+    return bsts_ioerror_known(BSTS_IOERR_AccessDenied, context);
+#endif
+#ifdef EEXIST
+  case EEXIST:
+    return bsts_ioerror_known(BSTS_IOERR_AlreadyExists, context);
+#endif
+#ifdef ENOTDIR
+  case ENOTDIR:
+    return bsts_ioerror_known(BSTS_IOERR_NotDirectory, context);
+#endif
+#ifdef EISDIR
+  case EISDIR:
+    return bsts_ioerror_known(BSTS_IOERR_IsDirectory, context);
+#endif
+#ifdef ENOTEMPTY
+  case ENOTEMPTY:
+    return bsts_ioerror_known(BSTS_IOERR_NotEmpty, context);
+#endif
+#ifdef EMFILE
+  case EMFILE:
+    return bsts_ioerror_known(BSTS_IOERR_TooManyOpenFiles, context);
+#endif
+#ifdef EROFS
+  case EROFS:
+    return bsts_ioerror_known(BSTS_IOERR_ReadOnlyFileSystem, context);
+#endif
+#ifdef EXDEV
+  case EXDEV:
+    return bsts_ioerror_known(BSTS_IOERR_CrossDeviceLink, context);
+#endif
+#ifdef ENOSPC
+  case ENOSPC:
+    return bsts_ioerror_known(BSTS_IOERR_NoSpace, context);
+#endif
+#ifdef EDQUOT
+  case EDQUOT:
+    return bsts_ioerror_known(BSTS_IOERR_QuotaExceeded, context);
+#endif
+#ifdef ENAMETOOLONG
+  case ENAMETOOLONG:
+    return bsts_ioerror_known(BSTS_IOERR_NameTooLong, context);
+#endif
+#ifdef EINVAL
+  case EINVAL:
+    return bsts_ioerror_known(BSTS_IOERR_InvalidArgument, context);
+#endif
+#ifdef EILSEQ
+  case EILSEQ:
+    return bsts_ioerror_known(BSTS_IOERR_InvalidUtf8, context);
+#endif
+#ifdef EBADF
+  case EBADF:
+    return bsts_ioerror_known(BSTS_IOERR_BadFileDescriptor, context);
+#endif
+#ifdef EINTR
+  case EINTR:
+    return bsts_ioerror_known(BSTS_IOERR_Interrupted, context);
+#endif
+#ifdef EAGAIN
+  case EAGAIN:
+    return bsts_ioerror_known(BSTS_IOERR_WouldBlock, context);
+#endif
+#if defined(EWOULDBLOCK) && (!defined(EAGAIN) || (EWOULDBLOCK != EAGAIN))
+  case EWOULDBLOCK:
+    return bsts_ioerror_known(BSTS_IOERR_WouldBlock, context);
+#endif
+#ifdef ETIMEDOUT
+  case ETIMEDOUT:
+    return bsts_ioerror_known(BSTS_IOERR_TimedOut, context);
+#endif
+#ifdef EPIPE
+  case EPIPE:
+    return bsts_ioerror_known(BSTS_IOERR_BrokenPipe, context);
+#endif
+#ifdef EOPNOTSUPP
+  case EOPNOTSUPP:
+    return bsts_ioerror_known(BSTS_IOERR_Unsupported, context);
+#endif
+#if defined(ENOTSUP) && (!defined(EOPNOTSUPP) || (ENOTSUP != EOPNOTSUPP))
+  case ENOTSUP:
+    return bsts_ioerror_known(BSTS_IOERR_Unsupported, context);
+#endif
+  default:
+    return bsts_ioerror_other(context, err, strerror(err));
+  }
+}
+
+static BValue bsts_ioerror_from_errno_default(int err, const char *context)
+{
+  if (err == 0)
+  {
+#ifdef EIO
+    err = EIO;
+#else
+    return bsts_ioerror_other(context, 0, "unknown error");
+#endif
+  }
+  return bsts_ioerror_from_errno(err, context);
+}
+
+static inline BValue bsts_ioerror_invalid_utf8(const char *context)
+{
+  return bsts_ioerror_known(BSTS_IOERR_InvalidUtf8, context);
+}
+
+static inline BValue bsts_ioerror_invalid_argument(const char *context)
+{
+  return bsts_ioerror_known(BSTS_IOERR_InvalidArgument, context);
+}
+
 
 BValue ___bsts_g_Bosatsu_l_Prog_l_pure(BValue a)
 {
@@ -38,13 +214,52 @@ BValue ___bsts_g_Bosatsu_l_Prog_l_flat__map(BValue p, BValue f)
 
 BValue bsts_print_effect(BValue a)
 {
-  bsts_string_print(a);
+  char *bytes = bsts_string_utf8_bytes(a);
+  size_t len = bsts_string_utf8_len(a);
+  if (len > 0)
+  {
+    errno = 0;
+    size_t written = fwrite(bytes, 1, len, stdout);
+    if (written < len)
+    {
+      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+          bsts_ioerror_from_errno_default(errno, "writing to stdout"));
+    }
+  }
+  if (fflush(stdout) != 0)
+  {
+    return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+        bsts_ioerror_from_errno_default(errno, "flushing stdout"));
+  }
   return ___bsts_g_Bosatsu_l_Prog_l_pure(bsts_unit_value());
 }
 
 BValue bsts_println_effect(BValue a)
 {
-  bsts_string_println(a);
+  char *bytes = bsts_string_utf8_bytes(a);
+  size_t len = bsts_string_utf8_len(a);
+  if (len > 0)
+  {
+    errno = 0;
+    size_t written = fwrite(bytes, 1, len, stdout);
+    if (written < len)
+    {
+      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+          bsts_ioerror_from_errno_default(errno, "writing to stdout"));
+    }
+  }
+  errno = 0;
+  size_t wrote_newline = fwrite("\n", 1, 1, stdout);
+  if (wrote_newline != 1)
+  {
+    return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+        bsts_ioerror_from_errno_default(errno, "writing newline to stdout"));
+  }
+  if (fflush(stdout) != 0)
+  {
+    return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+        bsts_ioerror_from_errno_default(errno, "flushing stdout"));
+  }
   return ___bsts_g_Bosatsu_l_Prog_l_pure(bsts_unit_value());
 }
 
@@ -84,9 +299,19 @@ bsts_utf8_is_valid_prefix(const char *data, int len)
 BValue bsts_read_stdin_utf8_bytes_effect(BValue size)
 {
   int requested = (int)bsts_integer_to_int32(size);
-  if (requested <= 0)
+  if (requested < 0)
   {
-    return ___bsts_g_Bosatsu_l_Prog_l_raise__error(bsts_unit_value());
+    char context[96];
+    snprintf(context, sizeof(context),
+             "read_stdin_utf8_bytes negative argument: %d",
+             requested);
+    return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+        bsts_ioerror_invalid_argument(context));
+  }
+  if (requested == 0)
+  {
+    // n=0 behaves like n=1 (there is no empty read command)
+    requested = 1;
   }
 
   /* We will never read more than requested + 4 bytes. */
@@ -94,7 +319,12 @@ BValue bsts_read_stdin_utf8_bytes_effect(BValue size)
   char *buf = (char *)malloc((size_t)capacity + 1); // +1 for NUL
   if (!buf)
   {
-    return ___bsts_g_Bosatsu_l_Prog_l_raise__error(bsts_unit_value());
+    int err = 0;
+#ifdef ENOMEM
+    err = ENOMEM;
+#endif
+    return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+        bsts_ioerror_other("reading from stdin", err, "out of memory"));
   }
 
   int len = 0;
@@ -111,10 +341,9 @@ BValue bsts_read_stdin_utf8_bytes_effect(BValue size)
     {
       if (ferror(stdin))
       {
-        // TODO: maybe we should return a better error than unit
-        // int err = errno ? errno : EIO;
         free(buf);
-        return ___bsts_g_Bosatsu_l_Prog_l_raise__error(bsts_unit_value());
+        return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+            bsts_ioerror_from_errno_default(errno, "reading from stdin"));
       }
       /* EOF */
       break;
@@ -143,7 +372,8 @@ BValue bsts_read_stdin_utf8_bytes_effect(BValue size)
     if (!bsts_utf8_is_valid_prefix(buf, len))
     {
       free(buf);
-      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(bsts_unit_value());
+      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+          bsts_ioerror_invalid_utf8("decoding bytes from stdin"));
     }
     buf[len] = '\0';
     BValue result = ___bsts_g_Bosatsu_l_Prog_l_pure(bsts_string_from_utf8_bytes_copy(len, buf));
@@ -168,7 +398,8 @@ BValue bsts_read_stdin_utf8_bytes_effect(BValue size)
     {
       /* We've already added 4 extra bytes and still not valid -> invalid UTF-8. */
       free(buf);
-      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(bsts_unit_value());
+      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+          bsts_ioerror_invalid_utf8("decoding bytes from stdin"));
     }
 
     int ch = fgetc(stdin);
@@ -176,14 +407,14 @@ BValue bsts_read_stdin_utf8_bytes_effect(BValue size)
     {
       if (ferror(stdin))
       {
-        // TODO: better error reporting
-        // int err = errno ? errno : EIO;
         free(buf);
-        return ___bsts_g_Bosatsu_l_Prog_l_raise__error(bsts_unit_value());
+        return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+            bsts_ioerror_from_errno_default(errno, "reading from stdin"));
       }
       /* EOF but current bytes don't end on a code point boundary -> invalid. */
       free(buf);
-      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(bsts_unit_value());
+      return ___bsts_g_Bosatsu_l_Prog_l_raise__error(
+          bsts_ioerror_invalid_utf8("decoding bytes from stdin"));
     }
 
     buf[len++] = (char)ch;
--- test_workspace/ProgExt.py
+++ test_workspace/ProgExt.py
@@ -7,8 +7,10 @@
 # ReadEnv() => (5, )
 # RemapEnv(p, f) => (6, p, f)
 
+import errno as _errno
+import os
 import sys
-from typing import Optional
+from typing import Optional, Tuple, Union
 
 def pure(a): return (0, a)
 def raise_error(e): return (1, e)
@@ -22,27 +24,115 @@ def effect(f): return (7, f)
 
 _pure_unit = pure(())
 
+_IOERR_NOT_FOUND = 0
+_IOERR_ACCESS_DENIED = 1
+_IOERR_ALREADY_EXISTS = 2
+_IOERR_NOT_DIRECTORY = 3
+_IOERR_IS_DIRECTORY = 4
+_IOERR_NOT_EMPTY = 5
+_IOERR_TOO_MANY_OPEN_FILES = 6
+_IOERR_READ_ONLY_FILESYSTEM = 7
+_IOERR_CROSS_DEVICE_LINK = 8
+_IOERR_NO_SPACE = 9
+_IOERR_QUOTA_EXCEEDED = 10
+_IOERR_NAME_TOO_LONG = 11
+_IOERR_INVALID_ARGUMENT = 12
+_IOERR_INVALID_UTF8 = 13
+_IOERR_BAD_FILE_DESCRIPTOR = 14
+_IOERR_INTERRUPTED = 15
+_IOERR_WOULD_BLOCK = 16
+_IOERR_TIMED_OUT = 17
+_IOERR_BROKEN_PIPE = 18
+_IOERR_UNSUPPORTED = 19
+_IOERR_OTHER = 20
+
+def _ioerr(tag: int, context: str):
+    return (tag, context)
+
+def _ioerror_other(context: str, code: int, message: str):
+    return (_IOERR_OTHER, context, code, message)
+
+def _ioerror_from_errno(err: Optional[int], context: str):
+    if err is None:
+        return _ioerror_other(context, 0, "unknown error")
+    if err == 0:
+        return _ioerror_other(context, 0, "unknown error")
+    if err == _errno.ENOENT:
+        return _ioerr(_IOERR_NOT_FOUND, context)
+    if err == _errno.EACCES:
+        return _ioerr(_IOERR_ACCESS_DENIED, context)
+    if err == _errno.EEXIST:
+        return _ioerr(_IOERR_ALREADY_EXISTS, context)
+    if err == _errno.ENOTDIR:
+        return _ioerr(_IOERR_NOT_DIRECTORY, context)
+    if err == _errno.EISDIR:
+        return _ioerr(_IOERR_IS_DIRECTORY, context)
+    if err == _errno.ENOTEMPTY:
+        return _ioerr(_IOERR_NOT_EMPTY, context)
+    if err == _errno.EMFILE:
+        return _ioerr(_IOERR_TOO_MANY_OPEN_FILES, context)
+    if err == _errno.EROFS:
+        return _ioerr(_IOERR_READ_ONLY_FILESYSTEM, context)
+    if err == _errno.EXDEV:
+        return _ioerr(_IOERR_CROSS_DEVICE_LINK, context)
+    if err == _errno.ENOSPC:
+        return _ioerr(_IOERR_NO_SPACE, context)
+    if hasattr(_errno, "EDQUOT") and err == _errno.EDQUOT:
+        return _ioerr(_IOERR_QUOTA_EXCEEDED, context)
+    if err == _errno.ENAMETOOLONG:
+        return _ioerr(_IOERR_NAME_TOO_LONG, context)
+    if err == _errno.EINVAL:
+        return _ioerr(_IOERR_INVALID_ARGUMENT, context)
+    if err == _errno.EILSEQ:
+        return _ioerr(_IOERR_INVALID_UTF8, context)
+    if err == _errno.EBADF:
+        return _ioerr(_IOERR_BAD_FILE_DESCRIPTOR, context)
+    if err == _errno.EINTR:
+        return _ioerr(_IOERR_INTERRUPTED, context)
+    if err == _errno.EAGAIN:
+        return _ioerr(_IOERR_WOULD_BLOCK, context)
+    if hasattr(_errno, "EWOULDBLOCK") and err == _errno.EWOULDBLOCK:
+        return _ioerr(_IOERR_WOULD_BLOCK, context)
+    if err == _errno.ETIMEDOUT:
+        return _ioerr(_IOERR_TIMED_OUT, context)
+    if err == _errno.EPIPE:
+        return _ioerr(_IOERR_BROKEN_PIPE, context)
+    if hasattr(_errno, "EOPNOTSUPP") and err == _errno.EOPNOTSUPP:
+        return _ioerr(_IOERR_UNSUPPORTED, context)
+    if hasattr(_errno, "ENOTSUP") and err == _errno.ENOTSUP:
+        return _ioerr(_IOERR_UNSUPPORTED, context)
+    return _ioerror_other(context, err, os.strerror(err))
+
+
 def println(s):
   def fn():
-    print(s)
-    return _pure_unit
+    try:
+      sys.stdout.write(s)
+      sys.stdout.write("\n")
+      sys.stdout.flush()
+      return _pure_unit
+    except OSError as exc:
+      return raise_error(_ioerror_from_errno(exc.errno, "writing to stdout"))
 
   return effect(fn)
 
 def print_stdout(s):
   def fn():
-    sys.stdout.write(s)
-    sys.stdout.flush()
-    return _pure_unit
+    try:
+      sys.stdout.write(s)
+      sys.stdout.flush()
+      return _pure_unit
+    except OSError as exc:
+      return raise_error(_ioerror_from_errno(exc.errno, "writing to stdout"))
 
   return effect(fn)
 
-def _read_utf8_chunk(requested: int) -> Optional[str]:
+def _read_utf8_chunk(requested: int) -> Tuple[bool, Union[str, tuple]]:
     """
     Read from standard input (or another binary stream) and return the shortest
     valid UTF-8 string whose byte length is:
 
-      - >= 0
+      - >= 1 (n=0 behaves like n=1)
       - <= requested + 4
 
     Behavior:
@@ -52,14 +142,20 @@ def _read_utf8_chunk(requested: int) -> Optional[str]:
     - We never drop bytes once read; if we return a string, it encodes back to
       exactly the bytes we consumed from the stream.
     - Any error (I/O error, still invalid UTF-8 after up to 4 extra bytes)
-      returns None.
+      returns (False, IOError).
     - If EOF is reached before `requested` bytes but the bytes we did get form
       valid UTF-8, we return that string.
-    - If we immediately see EOF, we return "".
+    - If we immediately see EOF, we return "" (the only way to get empty string).
     """
 
     if requested < 0:
-        return None
+        return (False, _ioerr(
+            _IOERR_INVALID_ARGUMENT,
+            f"read_stdin_utf8_bytes negative argument: {requested}"
+        ))
+    if requested == 0:
+        # n=0 behaves like n=1 (there is no empty read command)
+        requested = 1
 
     # Use the raw bytes interface to stdin
     stream = getattr(sys.stdin, "buffer", sys.stdin)
@@ -78,18 +174,18 @@ def _read_utf8_chunk(requested: int) -> Optional[str]:
         to_read = requested - len(buf)
         try:
             chunk = stream.read(to_read)
-        except OSError:
-            return None
+        except OSError as exc:
+            return (False, _ioerror_from_errno(exc.errno, "reading from stdin"))
 
         if chunk is None:
-            return None  # unexpected, treat as error
+            return (False, _ioerror_other("reading from stdin", 0, "unexpected None from read"))
 
         if not chunk:  # EOF
             break
 
         if not isinstance(chunk, (bytes, bytearray)):
             # Stream wasn't binary; this is a misuse, treat as error
-            return None
+            return (False, _ioerror_other("reading from stdin", 0, "stdin is not a binary stream"))
 
         buf.extend(chunk)
 
@@ -100,13 +196,13 @@ def _read_utf8_chunk(requested: int) -> Optional[str]:
         #   - Got exactly `requested` bytes and they’re valid
         #   - Got fewer than `requested` bytes due to EOF but still valid
         #   - requested == 0 -> empty buffer -> ""
-        return s
+        return (True, s)
 
     # 3. Decode failed.
     # If we didn't even reach `requested`, we hit EOF with invalid/truncated
     # UTF-8; nothing more to read, so treat as error.
     if len(buf) < requested:
-        return None
+        return (False, _ioerr(_IOERR_INVALID_UTF8, "decoding bytes from stdin"))
 
     # 4. We have at least `requested` bytes, but they don't decode yet.
     #    Assume we may have cut the last code point. Read up to 4 more bytes,
@@ -117,35 +213,35 @@ def _read_utf8_chunk(requested: int) -> Optional[str]:
     while extra < 4:
         try:
             chunk = stream.read(1)
-        except OSError:
-            return None
+        except OSError as exc:
+            return (False, _ioerror_from_errno(exc.errno, "reading from stdin"))
 
         if chunk is None:
-            return None
+            return (False, _ioerror_other("reading from stdin", 0, "unexpected None from read"))
 
         if not chunk:  # EOF
             break
 
         if not isinstance(chunk, (bytes, bytearray)):
-            return None
+            return (False, _ioerror_other("reading from stdin", 0, "stdin is not a binary stream"))
 
         buf.extend(chunk)
         extra += 1
 
         s = decode_buf()
         if s is not None:
-            return s
+            return (True, s)
 
     # Still invalid after up to 4 extra bytes → true UTF-8 error
-    return None
+    return (False, _ioerr(_IOERR_INVALID_UTF8, "decoding bytes from stdin"))
 
 def read_stdin_utf8_bytes(cnt):
   def fn():
-    string_or_none = _read_utf8_chunk(cnt)
-    if string_or_none is None:
-      return raise_error(())
+    ok, value = _read_utf8_chunk(cnt)
+    if ok:
+      return pure(value)
     else:
-      return pure(string_or_none)
+      return raise_error(value)
 
   return effect(fn)
 
