70ea5c641cdfdeaabfed35bca59f83ae7e573639
Oscar Boykin <oscar.boykin@gmail.com>
2026-02-15 08:20:05 -1000

    Improve Nullable helpers and tests

--- core/src/main/scala/dev/bosatsu/Nullable.scala
+++ core/src/main/scala/dev/bosatsu/Nullable.scala
@@ -1,8 +1,9 @@
 package dev.bosatsu
 
+import cats.Eq
 import scala.quoted.*
 
-opaque type Nullable[T] = T | Null
+opaque type Nullable[+T] = T | Null
 
 object Nullable:
 
@@ -11,10 +12,16 @@ object Nullable:
       ${ foldImpl('nullable, 'ifNull, 'fn) }
 
     inline def isNull: Boolean =
-      fold(true)(_ => false)
+      {
+        given CanEqual[T | Null, Null] = CanEqual.derived
+        nullable == null
+      }
 
     inline def nonNull: Boolean =
-      fold(false)(_ => true)
+      {
+        given CanEqual[T | Null, Null] = CanEqual.derived
+        nullable != null
+      }
 
     inline def map[B](inline fn: T => B): Nullable[B] =
       fold(null: Null)(fn)
@@ -31,13 +38,32 @@ object Nullable:
   inline def apply[A](inline a: A | Null): Nullable[A] =
     a
 
+  def fromOption[A](opt: Option[A]): Nullable[A] =
+    opt match
+      case Some(a) => a
+      case None    => null
+
+  given [A: Eq]: Eq[Nullable[A]] with
+    def eqv(na: Nullable[A], nb: Nullable[A]): Boolean =
+      val a: A | Null = na
+      val b: A | Null = nb
+      given CanEqual[A | Null, Null] = CanEqual.derived
+      if (a == null) b == null
+      else if (b == null) false
+      else Eq[A].eqv(a, b)
+
+  def empty[A]: Nullable[A] =
+    null
+
   private def foldImpl[T: Type, A: Type](
       nullable: Expr[Nullable[T]],
       ifNull: Expr[A],
       fn: Expr[T => A]
   )(using Quotes): Expr[A] =
+    val nullableUnion: Expr[T | Null] = '{ $nullable.asInstanceOf[T | Null] }
+
     '{
-      val n = $nullable
+      val n = $nullableUnion
       given CanEqual[T | Null, Null] = CanEqual.derived
       if (n == null) $ifNull
       else {
