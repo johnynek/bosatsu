2b3d8d495733616ca2a4479d39ce8229db92c450
P. Oscar Boykin <oscar.boykin@gmail.com>
2026-03-01 14:13:27 -1000

    Add --package-names output mode to lib show

--- core/src/main/scala/dev/bosatsu/library/Command.scala
+++ core/src/main/scala/dev/bosatsu/library/Command.scala
@@ -1663,6 +1663,12 @@ object Command {
             .orFalse,
           Opts
             .flag(
+              "package-names",
+              help = "show only package names in package entries"
+            )
+            .orFalse,
+          Opts
+            .flag(
               "no-opt",
               help = "disable normalization/optimization to inspect typed expressions before optimization"
             )
@@ -1683,6 +1689,7 @@ object Command {
               types,
               values,
               externalsOnly,
+              packageNamesOnly,
               noOpt,
               jsonOut,
               output,
@@ -1730,7 +1737,23 @@ object Command {
                 )
               } yield (
                 if (jsonOut)
-                  Output.JsonOutput(ShowEdn.showJson(packs, Nil), output)
+                  Output.JsonOutput(
+                    ShowEdn.showJson(
+                      packs,
+                      Nil,
+                      packageNamesOnly = packageNamesOnly
+                    ),
+                    output
+                  )
+                else if (packageNamesOnly)
+                  Output.Basic(
+                    ShowEdn.showDoc(
+                      packs,
+                      Nil,
+                      packageNamesOnly = true
+                    ),
+                    output
+                  )
                 else Output.ShowOutput(packs, Nil, output): Output[P]
               )
             }
