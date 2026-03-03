10366db809d33ccdac6821f3777df9114348b6b1
P. Oscar Boykin <oscar.boykin@gmail.com>
2026-03-02 16:54:25 -1000

    Add cmp_Char and range_Char exports with regression coverage

--- core/src/test/scala/dev/bosatsu/Issue1952Test.scala
+++ core/src/test/scala/dev/bosatsu/Issue1952Test.scala
@@ -15,6 +15,7 @@ class Issue1952Test extends munit.FunSuite with ParTest {
 package Repro/Issue1952
 
 from Bosatsu/Char import max_Char, char_to_Int, int_to_Char
+from Bosatsu/Char import cmp_Char, range_Char, char_List_to_Int_List
 
 def max_char_round_trip() -> Bool:
   match int_to_Char(1114111):
@@ -26,6 +27,8 @@ def max_char_round_trip() -> Bool:
 tests = TestSuite("Issue1952", [
   Assertion(eq_Int(char_to_Int(max_Char), 1114111), "max_Char codepoint"),
   Assertion(max_char_round_trip(), "max_Char round trip"),
+  Assertion(cmp_Char(.'a', .'b') matches LT, "cmp_Char export"),
+  Assertion(char_List_to_Int_List(range_Char(.'x', .'z')) matches [120, 121], "range_Char export"),
 ])
 """
 
@@ -33,7 +36,7 @@ tests = TestSuite("Issue1952", [
     runBosatsuTest(
       List(charPack, reproPack),
       "Repro/Issue1952",
-      2
+      4
     )
   }
 }
