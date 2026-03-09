5d18300e7bf14b81e5b57952bad6580052fdff13
P. Oscar Boykin <johnynek@users.noreply.github.com>
2026-03-08 18:17:19 -1000

    Fix #2082: avoid StackOverflowError in source-conversion type quantification (#2085)

--- core/src/test/scala/dev/bosatsu/ExprTest.scala
+++ core/src/test/scala/dev/bosatsu/ExprTest.scala
@@ -1,8 +1,9 @@
 package dev.bosatsu
 
-import cats.Eq
-import cats.data.NonEmptyList
+import cats.{Applicative, Eq}
+import cats.data.{Chain, NonEmptyList, Writer}
 import cats.syntax.all._
+import dev.bosatsu.rankn.Type
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Prop.forAll
 
@@ -15,6 +16,63 @@ class ExprTest extends munit.ScalaCheckSuite {
   val genExpr: Gen[Expr[Int]] = Generators.Exprs.gen(Gen.choose(0, 99), 4)
 
   implicit val arbExpr: Arbitrary[Expr[Int]] = Arbitrary(genExpr)
+
+  private def traverseTypeOracle[T, F[_]](
+      expr: Expr[T],
+      bound: Set[Type.Var.Bound]
+  )(
+      fn: (Type, Set[Type.Var.Bound]) => F[Type]
+  )(implicit F: Applicative[F]): F[Expr[T]] =
+    expr match {
+      case Expr.Annotation(e, tpe, a) =>
+        (traverseTypeOracle[T, F](e, bound)(fn), fn(tpe, bound))
+          .mapN(Expr.Annotation(_, _, a))
+      case v: Expr.Name[T]   => F.pure(v)
+      case Expr.App(f, args, t) =>
+        (
+          traverseTypeOracle[T, F](f, bound)(fn),
+          args.traverse(traverseTypeOracle[T, F](_, bound)(fn))
+        )
+          .mapN(Expr.App(_, _, t))
+      case Expr.Generic(bs, in) =>
+        val bound1 = bound ++ bs.toList.iterator.map(_._1)
+        traverseTypeOracle[T, F](in, bound1)(fn).map(Expr.Generic(bs, _))
+      case Expr.Lambda(args, expr, t) =>
+        (
+          args.traverse { case (n, optT) =>
+            optT.traverse(fn(_, bound)).map((n, _))
+          },
+          traverseTypeOracle[T, F](expr, bound)(fn)
+        ).mapN(Expr.Lambda(_, _, t))
+      case Expr.Let(arg, exp, in, rec, tag) =>
+        (
+          traverseTypeOracle[T, F](exp, bound)(fn),
+          traverseTypeOracle[T, F](in, bound)(fn)
+        )
+          .mapN(Expr.Let(arg, _, _, rec, tag))
+      case l @ Expr.Literal(_, _)   => F.pure(l)
+      case Expr.Match(arg, branches, tag) =>
+        val argB = traverseTypeOracle[T, F](arg, bound)(fn)
+        type B = Expr.Branch[T]
+        def branchFn(b: B): F[B] =
+          (
+            b.pattern.traverseType(fn(_, bound)),
+            b.guard.traverse(traverseTypeOracle[T, F](_, bound)(fn)),
+            traverseTypeOracle[T, F](b.expr, bound)(fn)
+          ).mapN { (pat, guard, expr) =>
+            Expr.Branch(pat, guard, expr)(using b.patternRegion)
+          }
+        val branchB = branches.traverse(branchFn)
+        (argB, branchB).mapN(Expr.Match(_, _, tag))
+    }
+
+  private def freeBoundTyVarsViaTraverseType[A](expr: Expr[A]): List[Type.Var.Bound] = {
+    val w = traverseTypeOracle(expr, Set.empty) { (t, bound) =>
+      val frees = Chain.fromSeq(Type.freeBoundTyVars(t :: Nil))
+      Writer(frees.filterNot(bound), t)
+    }
+    w.written.iterator.toList.distinct
+  }
 
   test("replaceTag replaces") {
     forAll { (s: Expr[Int], i: Int) =>
@@ -65,6 +123,15 @@ class ExprTest extends munit.ScalaCheckSuite {
     forAll(genExpr) { e =>
       val allNames = Expr.allNames(e)
       assert(e.freeVarsDup.forall(allNames))
+    }
+  }
+
+  test("freeBoundTyVars matches traverseType oracle") {
+    forAll(genExpr) { e =>
+      assertEquals(
+        Expr.freeBoundTyVars(e),
+        freeBoundTyVarsViaTraverseType(e)
+      )
     }
   }
 
