06e20a6d7491844fdec72d7c333ab93eb6fd760d
P. Oscar Boykin <oscar.boykin@gmail.com>
2026-03-17 16:08:40 -1000

    Add missing predef comparison/equality helpers

--- cli/src/test/scala/dev/bosatsu/codegen/clang/ClangGenTest.scala
+++ cli/src/test/scala/dev/bosatsu/codegen/clang/ClangGenTest.scala
@@ -42,7 +42,7 @@ class ClangGenTest extends munit.FunSuite {
       To inspect the code, change the hash, and it will print the code out
      */
     testFilesCompilesToHash("test_workspace/Ackermann.bosatsu")(
-      "bcd830f1232c65dc3f0c721989a12a94"
+      "6a6e1e5084006fdcd7abb76cd89e9277"
     )
   }
 }
