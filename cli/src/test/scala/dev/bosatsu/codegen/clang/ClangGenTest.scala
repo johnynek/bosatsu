e91ec66c2dc9d6e98fa84d9660bcd454e983f4d2
P. Oscar Boykin <johnynek@users.noreply.github.com>
2026-03-17 16:43:15 -1000

    Add boolean helpers to Bosatsu/Predef (#2198)

--- cli/src/test/scala/dev/bosatsu/codegen/clang/ClangGenTest.scala
+++ cli/src/test/scala/dev/bosatsu/codegen/clang/ClangGenTest.scala
@@ -42,7 +42,7 @@ class ClangGenTest extends munit.FunSuite {
       To inspect the code, change the hash, and it will print the code out
      */
     testFilesCompilesToHash("test_workspace/Ackermann.bosatsu")(
-      "bcd830f1232c65dc3f0c721989a12a94"
+      "9492efd0bbabd2b0e0fea2e85210cd6f"
     )
   }
 }
