addc4c510a34c0b7611e9bbc32a09d81bc5eae04
Oscar Boykin <oscar.boykin@gmail.com>
2026-03-18 19:57:45 -1000

    Revert "Stabilize CLI clang golden test"
    This reverts commit 32ad0cc47cac4a2546577522124e2610d48f3e6c.

--- cli/src/test/scala/dev/bosatsu/codegen/clang/ClangGenTest.scala
+++ cli/src/test/scala/dev/bosatsu/codegen/clang/ClangGenTest.scala
@@ -8,32 +8,7 @@ class ClangGenTest extends munit.FunSuite {
     val digest = md.digest(content.getBytes("UTF-8"))
     digest.map("%02x".format(_)).mkString
   }
-  private val syntheticIdReplacements =
-    List(
-      raw"__bsts_a_\d+" -> "__bsts_a_N",
-      raw"__bsts_l_[A-Za-z_]+\d+" -> "__bsts_l_IDN",
-      raw"__bsts_t_[A-Za-z_]+\d+" -> "__bsts_t_IDN",
-      raw"__bsts_b_[A-Za-z_]+\d+" -> "__bsts_b_IDN",
-      raw"__bstsi_slot\[\d+\]" -> "__bstsi_slot[I]"
-    )
-
-  def normalizeGeneratedC(fragment: String): String =
-    syntheticIdReplacements.foldLeft(fragment.replace("\r\n", "\n")) {
-      case (acc, (pat, repl)) => acc.replaceAll(pat, repl)
-    }
-
-  def blockStartingWith(rendered: String, prefix: String): String =
-    rendered
-      .strip
-      .split("\n\n")
-      .find(_.startsWith(prefix))
-      .getOrElse(fail(s"missing expected code block starting with: $prefix"))
-
-  def testFilesCompilesToHash(
-      path0: String,
-      blockPrefixes: List[String],
-      paths: String*
-  )(
+  def testFilesCompilesToHash(path0: String, paths: String*)(
       hashHex: String
   )(implicit loc: munit.Location) = Par.noParallelism {
     val pm: PackageMap.Typed[Any] = TestUtils.compileFile(path0, paths*)
@@ -52,12 +27,7 @@ class ClangGenTest extends munit.FunSuite {
     res match {
       case Right(d) =>
         val everything = d.render(80)
-        val normalized = normalizeGeneratedC(
-          blockPrefixes.iterator
-            .map(blockStartingWith(everything, _))
-            .mkString("", "\n\n", "\n")
-        )
-        val hashed = md5HashToHex(normalized)
+        val hashed = md5HashToHex(everything)
         assertEquals(
           hashed,
           hashHex,
@@ -71,14 +41,8 @@ class ClangGenTest extends munit.FunSuite {
     /*
       To inspect the code, change the hash, and it will print the code out
      */
-    testFilesCompilesToHash(
-      "test_workspace/Ackermann.bosatsu",
-      List(
-        "BValue ___bsts_g_Ackermann_l_ack(",
-        "int main("
-      )
-    )(
-      "64d9073ab16e7936fde71bf62db5e124"
+    testFilesCompilesToHash("test_workspace/Ackermann.bosatsu")(
+      "8423c08f796f8d06e4839aebc70326ae"
     )
   }
 }
