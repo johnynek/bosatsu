cd17d48064253ed92bd8136d0bb6b87b10039e1e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-04-07 01:41:45 +0200

    Update sbt-scalajs, scalajs-library_2.13, ... to 1.21.0 (#2303)

--- jsui/webpack.config.js
+++ jsui/webpack.config.js
@@ -1,7 +1,22 @@
 const scalajsWebpackConfig = require("./scalajs.webpack.config.js");
+const generatedScalaJsBundlePattern = /compiler-jsui-(fastopt|opt)\.js$/;
+// Scala.js 1.21 emits bundle mappings that webpack's source-map-loader rejects.
+const scalajsRules = ((scalajsWebpackConfig.module || {}).rules || []).map(
+  (rule) =>
+    rule.enforce === "pre" && JSON.stringify(rule.use).includes("source-map-loader")
+      ? {
+          ...rule,
+          exclude: generatedScalaJsBundlePattern
+        }
+      : rule
+);
 
 module.exports = {
   ...scalajsWebpackConfig,
+  module: {
+    ...(scalajsWebpackConfig.module || {}),
+    rules: scalajsRules
+  },
   resolve: {
     ...(scalajsWebpackConfig.resolve || {}),
     fallback: {
