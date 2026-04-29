d811e1d32de09f30008733ded6fd0e3a64b63774
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-04-29 23:24:11 +0200

    Update protobuf-java to 3.25.9 (#2346)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.3.0")
   lazy val paiges = Def.setting("org.typelevel" %%% "paiges-core" % "0.4.4")
   lazy val protobufJava =
-    Def.setting("com.google.protobuf" % "protobuf-java" % "3.25.8")
+    Def.setting("com.google.protobuf" % "protobuf-java" % "3.25.9")
   lazy val protobufJavaUtil =
     Def.setting("com.google.protobuf" % "protobuf-java-util" % "3.25.8")
   lazy val scalaCheck =
