9f4203c159790f83d824a7e0f4d956f89c113d44
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-12 21:52:33 +0200

    Update scalacheck to 1.20.0 (#2416)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
   lazy val protobufJavaUtil =
     Def.setting("com.google.protobuf" % "protobuf-java-util" % "4.36.1")
   lazy val scalaCheck =
-    Def.setting("org.scalacheck" %%% "scalacheck" % "1.19.0")
+    Def.setting("org.scalacheck" %%% "scalacheck" % "1.20.0")
   lazy val scalawasiz3 = Def.setting("dev.bosatsu" %%% "scalawasiz3" % "0.0.13")
   lazy val slf4jNop = Def.setting("org.slf4j" % "slf4j-nop" % "2.0.19")
 }
